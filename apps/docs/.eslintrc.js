a1cdddc70b9f1925154d7af5c71359e2388b629b
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-03-17 15:41:07 +0700

    Add Storybook configuration and files

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@codefast/eslint-config/next.js"],
+  extends: ["@codefast/eslint-config/next.js", "plugin:storybook/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
