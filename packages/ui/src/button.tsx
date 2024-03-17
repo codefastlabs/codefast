71b114ac63a73f3a87506b30380b8653439270e3
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-03-17 22:42:05 +0700

    Refactor button component and remove unused utility function

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,26 +1,67 @@
-"use client";
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { type VariantProps } from "cva";
+import { cva } from "./cva";
 
-import { cn } from "./utils";
+/* -----------------------------------------------------------------------------
+ * Variant: Button
+ * -------------------------------------------------------------------------- */
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  /* The name of the app that the button is in. */
-  appName: string;
+const buttonVariants = cva({
+  base: "focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50",
+  variants: {
+    variant: {
+      default: "bg-primary text-primary-foreground hover:bg-primary/90 shadow",
+      destructive:
+        "bg-destructive text-destructive-foreground hover:bg-destructive/90 shadow-sm",
+      ghost: "hover:bg-accent hover:text-accent-foreground",
+      link: "text-primary underline-offset-4 hover:underline",
+      outline:
+        "border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm",
+      secondary:
+        "bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm",
+    },
+    size: {
+      default: "h-9 px-4",
+      lg: "h-10 px-8",
+      sm: "h-8 px-3 text-xs",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+    variant: "default",
+  },
+});
+
+/* -----------------------------------------------------------------------------
+ * Component: Button
+ * -------------------------------------------------------------------------- */
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
 }
 
-export function Button({
-  appName,
-  className,
-  ...props
-}: ButtonProps): React.JSX.Element {
-  return (
-    <button
-      className={cn("rounded bg-sky-700 px-4 py-2", className)}
-      onClick={() => {
-        // eslint-disable-next-line -- no-alert
-        alert(`Hello from your ${appName} app!`);
-      }}
-      type="button"
-      {...props}
-    />
-  );
-}
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+
+    return (
+      <Comp
+        ref={ref}
+        type={asChild ? undefined : "button"}
+        className={buttonVariants({ variant, size, className })}
+        {...props}
+      />
+    );
+  },
+);
+
+Button.displayName = "Button";
+
+/* -----------------------------------------------------------------------------
+ * Export
+ * -------------------------------------------------------------------------- */
+
+export { Button, buttonVariants, type ButtonProps };
