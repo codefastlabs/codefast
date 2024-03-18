1961854e575e36b4a99a6c64452481e59c30958c
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-03-18 08:13:24 +0700

    Add alert component and remove card component

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -61,7 +61,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 Button.displayName = "Button";
 
 /* -----------------------------------------------------------------------------
- * Export
+ * Exports
  * -------------------------------------------------------------------------- */
 
 export { Button, buttonVariants, type ButtonProps };
