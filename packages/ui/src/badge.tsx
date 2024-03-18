424a276bd666438304a97e20843a8c911a9621e2
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-03-18 10:57:39 +0700

    Update package.json and UI components

--- packages/ui/src/badge.tsx
+++ packages/ui/src/badge.tsx
@@ -1,9 +1,9 @@
+import { type VariantProps } from "cva";
+import { cva } from "./cva";
+
 /* -----------------------------------------------------------------------------
  * Variant: Badge
  * -------------------------------------------------------------------------- */
-
-import { type VariantProps } from "cva";
-import { cva } from "./cva";
 
 const badgeVariants = cva({
   base: "focus:ring-ring inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2",
---
+++ packages/ui/src/breadcrumb.tsx
@@ -0,0 +1,167 @@
+import { ChevronRightIcon, DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { Slot } from "@radix-ui/react-slot";
+import { forwardRef } from "react";
+import { cx } from "./cva";
+
+/* -----------------------------------------------------------------------------
+ * Component: Breadcrumb
+ * -------------------------------------------------------------------------- */
+
+interface BreadcrumbProps extends React.HTMLAttributes<HTMLElement> {
+  separator?: React.ReactNode;
+}
+
+const Breadcrumb = forwardRef<HTMLElement, BreadcrumbProps>(
+  ({ ...props }, ref) => <nav ref={ref} aria-label="breadcrumb" {...props} />,
+);
+
+Breadcrumb.displayName = "Breadcrumb";
+
+/* -----------------------------------------------------------------------------
+ * Component: BreadcrumbList
+ * -------------------------------------------------------------------------- */
+
+type BreadcrumbListProps = React.OlHTMLAttributes<HTMLOListElement>;
+
+const BreadcrumbList = forwardRef<HTMLOListElement, BreadcrumbListProps>(
+  ({ className, ...props }, ref) => (
+    <ol
+      ref={ref}
+      className={cx(
+        "text-muted-foreground flex flex-wrap items-center gap-1.5 break-words text-sm sm:gap-2.5",
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+
+BreadcrumbList.displayName = "BreadcrumbList";
+
+/* -----------------------------------------------------------------------------
+ * Component: BreadcrumbItem
+ * -------------------------------------------------------------------------- */
+
+type BreadcrumbItemProps = React.LiHTMLAttributes<HTMLLIElement>;
+
+const BreadcrumbItem = forwardRef<HTMLLIElement, BreadcrumbItemProps>(
+  ({ className, ...props }, ref) => (
+    <li
+      ref={ref}
+      className={cx("inline-flex items-center gap-1.5", className)}
+      {...props}
+    />
+  ),
+);
+
+BreadcrumbItem.displayName = "BreadcrumbItem";
+
+/* -----------------------------------------------------------------------------
+ * Component: BreadcrumbLink
+ * -------------------------------------------------------------------------- */
+
+interface BreadcrumbLinkProps
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  asChild?: boolean;
+}
+
+const BreadcrumbLink = forwardRef<HTMLAnchorElement, BreadcrumbLinkProps>(
+  ({ asChild, className, ...props }, ref) => {
+    const Comp = asChild ? Slot : "a";
+
+    return (
+      <Comp
+        ref={ref}
+        className={cx("hover:text-foreground transition-colors", className)}
+        {...props}
+      />
+    );
+  },
+);
+
+BreadcrumbLink.displayName = "BreadcrumbLink";
+
+/* -----------------------------------------------------------------------------
+ * Component: BreadcrumbPage
+ * -------------------------------------------------------------------------- */
+
+type BreadcrumbPageProps = React.HTMLAttributes<HTMLSpanElement>;
+
+const BreadcrumbPage = forwardRef<HTMLSpanElement, BreadcrumbPageProps>(
+  ({ className, ...props }, ref) => (
+    <span
+      ref={ref}
+      role="link"
+      aria-disabled="true"
+      aria-current="page"
+      className={cx("text-foreground font-normal", className)}
+      {...props}
+    />
+  ),
+);
+
+BreadcrumbPage.displayName = "BreadcrumbPage";
+
+/* -----------------------------------------------------------------------------
+ * Component: BreadcrumbSeparator
+ * -------------------------------------------------------------------------- */
+
+type BreadcrumbSeparatorProps = React.LiHTMLAttributes<HTMLLIElement>;
+
+function BreadcrumbSeparator({
+  children,
+  className,
+  ...props
+}: BreadcrumbSeparatorProps): React.JSX.Element {
+  return (
+    <li
+      role="presentation"
+      aria-hidden="true"
+      className={cx("[&>svg]:size-3.5", className)}
+      {...props}
+    >
+      {children ?? <ChevronRightIcon />}
+    </li>
+  );
+}
+
+BreadcrumbSeparator.displayName = "BreadcrumbSeparator";
+
+/* -----------------------------------------------------------------------------
+ * Component: BreadcrumbEllipsis
+ * -------------------------------------------------------------------------- */
+
+type BreadcrumbEllipsisProps = React.HTMLAttributes<HTMLSpanElement>;
+
+function BreadcrumbEllipsis({
+  className,
+  ...props
+}: BreadcrumbEllipsisProps): React.JSX.Element {
+  return (
+    <span
+      role="presentation"
+      aria-hidden="true"
+      className={cx("flex size-9 items-center justify-center", className)}
+      {...props}
+    >
+      <DotsHorizontalIcon className="size-4" />
+      <span className="sr-only">More</span>
+    </span>
+  );
+}
+
+BreadcrumbEllipsis.displayName = "BreadcrumbElipssis";
+
+/* -----------------------------------------------------------------------------
+ * Exports
+ * -------------------------------------------------------------------------- */
+
+export {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  BreadcrumbEllipsis,
+};
