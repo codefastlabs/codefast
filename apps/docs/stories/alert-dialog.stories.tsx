fdbd91763712a02492cc8164032467d77e0a5e75
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-03-18 13:02:04 +0700

    Update tailwind.config.ts and package.json, add AlertDialog component

---
+++ apps/docs/stories/alert-dialog.stories.tsx
@@ -0,0 +1,46 @@
+import { type Meta, type StoryObj } from "@storybook/react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@codefast/ui/alert-dialog";
+import { Button } from "@codefast/ui/button";
+
+const meta = {
+  component: AlertDialog,
+  tags: ["autodocs"],
+  title: "UIs/Alert Dialog",
+} satisfies Meta<typeof AlertDialog>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => (
+    <AlertDialog {...args}>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Show Dialog</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  ),
+};
