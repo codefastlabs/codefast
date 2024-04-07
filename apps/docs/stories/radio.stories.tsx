f895b6585e7b2752e551371ebc016c55a14f3a41
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-04-07 22:59:41 +0700

    Add new UI stories for various components
    Introduced UI stories demonstrating the usage of several components. Added stories for CheckBoxGroup, CheckboxCards, RadioCards, Radio, and Spinner within the Docs app. The stories showcase component's default behavior and also some more complex use-cases like integration with React Hook Form.

---
+++ apps/docs/stories/radio.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Radio } from "@codefast/ui/radio";
+import { Box } from "@codefast/ui/box";
+import { Label } from "@codefast/ui/label";
+
+const meta = {
+  component: Radio,
+  tags: ["autodocs"],
+  title: "UIs/Radio",
+} satisfies Meta<typeof Radio>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+/* -----------------------------------------------------------------------------
+ * Story: Default
+ * -------------------------------------------------------------------------- */
+
+export const Default: Story = {
+  render: () => (
+    <Box className="flex flex-col gap-2">
+      <Box asChild className="flex items-center gap-2">
+        <Label>
+          <Radio name="example" value="1" defaultChecked />
+          Default
+        </Label>
+      </Box>
+      <Box asChild className="flex items-center gap-2">
+        <Label>
+          <Radio name="example" value="2" />
+          Comfortable
+        </Label>
+      </Box>
+      <Box asChild className="flex items-center gap-2">
+        <Label>
+          <Radio name="example" value="3" />
+          Compact
+        </Label>
+      </Box>
+      <Box asChild className="flex items-center gap-2">
+        <Label>
+          <Radio name="example" value="4" disabled />
+          Disabled
+        </Label>
+      </Box>
+    </Box>
+  ),
+};
---
+++ apps/docs/stories/spinner.stories.tsx
@@ -0,0 +1,69 @@
+import { Button } from "@codefast/ui/button";
+import { Switch } from "@codefast/ui/switch";
+import { BookmarkIcon } from "@radix-ui/react-icons";
+import type { Meta, StoryObj } from "@storybook/react";
+import { Spinner } from "@codefast/ui/spinner";
+import { Box } from "@codefast/ui/box";
+
+const meta = {
+  component: Spinner,
+  tags: ["autodocs"],
+  title: "UIs/Spinner",
+} satisfies Meta<typeof Spinner>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+/* -----------------------------------------------------------------------------
+ * Story: Default
+ * -------------------------------------------------------------------------- */
+
+export const Default: Story = {};
+
+/* -----------------------------------------------------------------------------
+ * Story: Size
+ * -------------------------------------------------------------------------- */
+
+export const Size: Story = {
+  render: () => (
+    <Box className="flex items-center gap-4">
+      <Spinner />
+      <Spinner className="size-6" />
+      <Spinner className="size-8" />
+    </Box>
+  ),
+};
+
+/* -----------------------------------------------------------------------------
+ * Story: With Children
+ * -------------------------------------------------------------------------- */
+
+export const WithChildren: Story = {
+  render: () => (
+    <Box className="flex items-center gap-4">
+      <Spinner loading>
+        <Switch defaultChecked />
+      </Spinner>
+
+      <Spinner loading={false}>
+        <Switch defaultChecked />
+      </Spinner>
+    </Box>
+  ),
+};
+
+/* -----------------------------------------------------------------------------
+ * Story: With Button
+ * -------------------------------------------------------------------------- */
+
+export const WithButton: Story = {
+  render: () => (
+    <Button disabled>
+      <Spinner loading>
+        <BookmarkIcon />
+      </Spinner>
+      Bookmark
+    </Button>
+  ),
+};
