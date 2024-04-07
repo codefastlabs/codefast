059c2075b77d57a499311e4e2a10bd7ab26a1c76
BM Valentino <valentino@brickmate.vn>
2024-04-07 22:59:41 +0700

    Add new UI stories for various components
    Introduced UI stories demonstrating the usage of several components. Added stories for CheckBoxGroup, CheckboxCards, RadioCards, Radio, and Spinner within the Docs app. The stories showcase component's default behavior and also some more complex use-cases like integration with React Hook Form.

---
+++ apps/docs/stories/checkbox-cards.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { CheckboxCards, CheckboxCardsItem } from "@codefast/ui/checkbox-cards";
+import { Box } from "@codefast/ui/box";
+import { Text } from "@codefast/ui/text";
+
+const meta = {
+  component: CheckboxCards,
+  tags: ["autodocs"],
+  title: "UIs/Checkbox Cards",
+} satisfies Meta<typeof CheckboxCards>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+/* -----------------------------------------------------------------------------
+ * Story: Default
+ * -------------------------------------------------------------------------- */
+
+export const Default: Story = {
+  render: (args) => (
+    <Box>
+      <CheckboxCards defaultValue={["1"]} className="grid-cols-1 sm:grid-cols-3" {...args}>
+        <CheckboxCardsItem value="1">
+          <Box className="flex w-full flex-col items-start">
+            <Text className="font-bold">8-core CPU</Text>
+            <Text>32GB RAM</Text>
+          </Box>
+        </CheckboxCardsItem>
+        <CheckboxCardsItem value="2">
+          <Box className="flex w-full flex-col items-start">
+            <Text className="font-bold">6-core CPU</Text>
+            <Text>24GB RAM</Text>
+          </Box>
+        </CheckboxCardsItem>
+        <CheckboxCardsItem value="3">
+          <Box className="flex w-full flex-col items-start">
+            <Text className="font-bold">4-core CPU</Text>
+            <Text>16GB RAM</Text>
+          </Box>
+        </CheckboxCardsItem>
+      </CheckboxCards>
+    </Box>
+  ),
+};
