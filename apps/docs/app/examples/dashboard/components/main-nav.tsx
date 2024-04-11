63f95bedf74ae3a0e038f00d3ccaeba157959168
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-04-11 22:19:44 +0700

    Add dashboard UI for task and sales management
    This commit introduces a comprehensive set of UI components and populates them with mock data for a dashboard-style application. Components include date-range picker, search, team-switcher and myriad widgets displaying charts and sales data. Unused attributes and refactored components have been removed for clean, understandable and modular code.

---
+++ apps/docs/app/examples/dashboard/components/main-nav.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { cn } from "@codefast/ui/utils";
+import { type HTMLAttributes, type JSX } from "react";
+
+export function MainNav({ className, ...props }: HTMLAttributes<HTMLElement>): JSX.Element {
+  return (
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)} {...props}>
+      <Link href="/examples/dashboard" className="hover:text-primary text-sm font-medium transition-colors">
+        Overview
+      </Link>
+      <Link
+        href="/examples/dashboard"
+        className="text-muted-foreground hover:text-primary text-sm font-medium transition-colors"
+      >
+        Customers
+      </Link>
+      <Link
+        href="/examples/dashboard"
+        className="text-muted-foreground hover:text-primary text-sm font-medium transition-colors"
+      >
+        Products
+      </Link>
+      <Link
+        href="/examples/dashboard"
+        className="text-muted-foreground hover:text-primary text-sm font-medium transition-colors"
+      >
+        Settings
+      </Link>
+    </nav>
+  );
+}
---
+++ apps/docs/app/examples/dashboard/components/search.tsx
@@ -0,0 +1,10 @@
+import { Input } from "@codefast/ui/input";
+import { type JSX } from "react";
+
+export function Search(): JSX.Element {
+  return (
+    <div>
+      <Input type="search" placeholder="Search..." className="md:w-[100px] lg:w-[300px]" />
+    </div>
+  );
+}
