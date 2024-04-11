127f967f451d88e86a3d2ae6f76f628e61fc7a74
BM Valentino <valentino@brickmate.vn>
2024-04-11 21:55:20 +0700

    Add task management example with data generation and UI components
    This commit introduces a task management example that contains task data generation and various related UI components. It includes a table to list tasks, allowing tasks to be sorted and filtered, and actions for each task such as editing and deleting. A user navigation menu has also been added for interaction with the data. A custom schema is used for data validation.

---
+++ apps/docs/app/examples/tasks/page.tsx
@@ -0,0 +1,61 @@
+import fs from "node:fs";
+import path from "node:path";
+import { type Metadata } from "next";
+import { z } from "zod";
+import Image from "next/image";
+import { type JSX } from "react";
+import { DataTable } from "@/app/examples/tasks/components/data-table";
+import { type Task, taskSchema } from "@/app/examples/tasks/data/schema";
+import { UserNav } from "@/app/examples/tasks/components/user-nav";
+import { columns } from "@/app/examples/tasks/components/columns";
+
+export const metadata: Metadata = {
+  title: "Tasks",
+  description: "A task and issue tracker build using Tanstack Table.",
+};
+
+// Simulate a database read for tasks.
+async function getTasks(): Promise<Task[]> {
+  const data = fs.readFileSync(path.join(process.cwd(), "app/examples/tasks/data/tasks.json"));
+
+  const tasks = JSON.parse(data.toString()) as Task[];
+
+  return Promise.resolve(z.array(taskSchema).parse(tasks));
+}
+
+export default async function TaskPage(): Promise<JSX.Element> {
+  const tasks = await getTasks();
+
+  return (
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/examples/tasks-light.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/tasks-dark.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">Here&apos;s a list of your tasks for this month!</p>
+          </div>
+          <div className="flex items-center space-x-2">
+            <UserNav />
+          </div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
+  );
+}
