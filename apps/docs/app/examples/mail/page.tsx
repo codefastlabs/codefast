c9e35208c9de14287b8b473a5eda4776006577cf
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-04-12 22:40:32 +0700

    Add account switcher component and update mail app dependencies
    Introduced a new account switcher component to the mail app along with updating the mail app dependencies versions. Also created new components for navigation purposes and a new hook for managing mails. These changes should improve the user experience when interacting with multiple accounts and enhance the performance due to updated dependencies.

---
+++ apps/docs/app/examples/mail/page.tsx
@@ -0,0 +1,31 @@
+import { cookies } from "next/headers";
+import Image from "next/image";
+import { type JSX } from "react";
+import { Mail } from "@/app/examples/mail/components/mail";
+import { accounts, mails } from "@/app/examples/mail/data/data";
+
+export default function MailPage(): JSX.Element {
+  const layout = cookies().get("react-resizable-panels:layout");
+  const collapsed = cookies().get("react-resizable-panels:collapsed");
+
+  const defaultLayout: number[] = layout ? JSON.parse(layout.value) : undefined;
+  const defaultCollapsed: boolean = collapsed ? JSON.parse(collapsed.value) : undefined;
+
+  return (
+    <>
+      <div className="md:hidden">
+        <Image src="/examples/mail-dark.png" width={1280} height={727} alt="Mail" className="hidden dark:block" />
+        <Image src="/examples/mail-light.png" width={1280} height={727} alt="Mail" className="block dark:hidden" />
+      </div>
+      <div className="hidden flex-col md:flex">
+        <Mail
+          accounts={accounts}
+          mails={mails}
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+          navCollapsedSize={4}
+        />
+      </div>
+    </>
+  );
+}
