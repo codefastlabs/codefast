640b04ea0d34815e803baa92c0678ee1f7e0d93a
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-04-15 23:33:36 +0700

    Add useMutationObserver hook to docs
    A new file 'use-mutation-observer.ts' has been created in the hooks directory under docs. This file introduces a useMutationObserver hook that makes use of the MutationObserver API to observe changes in the DOM. The hook accepts an element reference, a callback function, and observer options as parameters.

---
+++ apps/docs/hooks/use-mutation-observer.ts
@@ -0,0 +1,25 @@
+import { type MutableRefObject, useEffect } from "react";
+
+export const useMutationObserver = (
+  ref: MutableRefObject<HTMLElement | null>,
+  callback: MutationCallback,
+  options = {
+    attributes: true,
+    characterData: true,
+    childList: true,
+    subtree: true,
+  },
+): void => {
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    const observer = new MutationObserver(callback);
+    observer.observe(ref.current, options);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref, callback, options]);
+};
