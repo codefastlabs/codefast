5018f93a73f38f89a87f44f811da434d810b08a7
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-04-18 20:32:35 +0700

    Update ESLint configuration in docs app
    The changes in this commit include the addition of "eslint-config-turbo" in the extends field of the ESLint configuration file in the docs app. This ensures that the linting rules for the docs app now also include the ones specified in "eslint-config-turbo".

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -1,6 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@codefast/eslint-config/next", "plugin:storybook/recommended"],
+  extends: ["@codefast/eslint-config/next", "plugin:storybook/recommended", "eslint-config-turbo"],
   ignorePatterns: ["storybook-static"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
