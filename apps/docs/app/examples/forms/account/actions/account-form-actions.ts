39e6e98ab3f0d83496d5a378fee0343b4c7cd0ac
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-04-20 22:48:30 +0700

    Update account form functionality and CSS adjustments
    Improved the account form functionality by creating a new file, `account-form-actions.ts`, for handling form submission and error cases. Moved `accountFormSchema` to its own dedicated file to enhance code organization. Adjusted CSS values for `--destructive` and `--destructive-foreground` in `globals.css` to alter the visual design.

---
+++ apps/docs/app/examples/forms/account/actions/account-form-actions.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { wait } from "next/dist/lib/wait";
+import { type typeToFlattenedError as TypeToFlattenedError } from "zod";
+import { accountFormSchema, type AccountFormValues } from "@/app/examples/forms/account/schemata/account-form-schema";
+
+export interface FormState {
+  errors?: TypeToFlattenedError<AccountFormValues>["fieldErrors"];
+  message: string;
+  success: boolean;
+}
+
+export default async function updateAccount(data: AccountFormValues): Promise<FormState> {
+  try {
+    const validatedFields = accountFormSchema.safeParse(data);
+
+    // If the fields are not valid, return the errors.
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: "There are errors in the form.",
+        success: false,
+      };
+    }
+
+    // Update the account.
+    await wait(100);
+
+    return {
+      message: "Account updated successfully!",
+      success: true,
+    };
+  } catch (error) {
+    return {
+      message: "An error occurred while updating the account.",
+      success: false,
+    };
+  }
+}
---
+++ apps/docs/app/examples/forms/account/schemata/account-form-schema.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const accountFormSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(2, {
+      message: "Name must be at least 2 characters.",
+    })
+    .max(30, {
+      message: "Name must not be longer than 30 characters.",
+    }),
+  dob: z.coerce.date({
+    required_error: "A date of birth is required.",
+  }),
+  language: z
+    .string({
+      required_error: "Please select a language.",
+    })
+    .trim(),
+});
+
+export type AccountFormValues = z.infer<typeof accountFormSchema>;
