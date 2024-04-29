7a07e92eed4026f1f2face1cfeb2dc0323b65039
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-04-29 16:01:36 +0700

    Update icon size declaration in multiple files
    Changed the CSS classname used for defining icon size. The classname has been updated from "h-4 w-4" to a more reusable and simpler classname "size-4". This change has been made across various components and stories within the "apps/docs" directory for a more consistent and cleaner codebase.

--- apps/docs/stories/card.stories.tsx
+++ apps/docs/stories/card.stories.tsx
@@ -103,7 +103,7 @@ export const Examples: Story = {
         <Box>
           {notifications.map((notification) => (
             <Box key={notification.id} className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-              <Box as="span" className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+              <Box as="span" className="flex size-2 translate-y-1 rounded-full bg-sky-500" />
               <Box className="space-y-1">
                 <Text className="text-sm font-medium leading-none">{notification.title}</Text>
                 <Text className="text-muted-foreground text-sm">{notification.description}</Text>
