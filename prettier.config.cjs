718e071e02c066c66f7962333d147d95ed9fc571
BM Valentino <valentino@brickmate.vn>
2024-05-05 13:13:54 +0700

    Migrate prettier config to package.json and update code formatting rules
    The code formatting rules have been updated in the code styles and the prettier configuration has been moved into package.json. A new dependency, `prettier-plugin-packagejson`, has been added to support the changes.

--- prettier.config.cjs
+++
@@ -1,7 +0,0 @@
-const styleguide = require('@vercel/style-guide/prettier');
-
-/** @type {import('prettier').Config} */
-module.exports = {
-  ...styleguide,
-  plugins: [styleguide.plugins, 'prettier-plugin-tailwindcss'],
-};
