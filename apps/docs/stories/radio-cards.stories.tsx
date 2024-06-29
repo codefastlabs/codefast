4303b3b10dcef1ccf1d72af722f9a2118634900e
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-06-29 17:29:59 +0700

    Update storybook commands and add hover effects
    Modified the "dev-storybook" command in package.json for improved execution. Also added hover effects to RadioCardsItem components in the radio-cards story by introducing "hover:bg-accent" and "hover:text-accent-foreground" classes. This aids user interaction experience.

--- apps/docs/stories/radio-cards.stories.tsx
+++ apps/docs/stories/radio-cards.stories.tsx
@@ -21,19 +21,19 @@ export const Default: Story = {
   render: (args) => (
     <Box>
       <RadioCards defaultValue="1" className="grid-cols-1 sm:grid-cols-3" {...args}>
-        <RadioCardsItem value="1">
+        <RadioCardsItem value="1" className="hover:bg-accent hover:text-accent-foreground">
           <Box className="flex w-full flex-col items-start">
             <Text className="font-bold">8-core CPU</Text>
             <Text>32GB RAM</Text>
           </Box>
         </RadioCardsItem>
-        <RadioCardsItem value="2">
+        <RadioCardsItem value="2" className="hover:bg-accent hover:text-accent-foreground">
           <Box className="flex w-full flex-col items-start">
             <Text className="font-bold">6-core CPU</Text>
             <Text>24GB RAM</Text>
           </Box>
         </RadioCardsItem>
-        <RadioCardsItem value="3">
+        <RadioCardsItem value="3" className="hover:bg-accent hover:text-accent-foreground">
           <Box className="flex w-full flex-col items-start">
             <Text className="font-bold">4-core CPU</Text>
             <Text>16GB RAM</Text>
