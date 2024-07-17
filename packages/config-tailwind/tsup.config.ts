d468f77105ec5c1f3156944190b2ae744891667c
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-07-17 21:14:41 +0700

    Update ESLint config and refactor dependencies in config-tailwind
    This commit changes the extended ESLint configuration from '@codefast/eslint-config/react' to '@codefast/eslint-config/library' in config-tailwind. It also reorganizes dependencies, where "tailwindcss" has been moved from "dependencies" to "devDependencies". In addition, the "baseUrl" has been set and the files to include are updated in the TypeScript configuration.

--- packages/config-tailwind/tsup.config.ts
+++ packages/config-tailwind/tsup.config.ts
@@ -5,6 +5,7 @@ export default defineConfig((options) => [
     clean: !options.watch,
     dts: true,
     entry: ['src/**/*.ts'],
+    external: ['tailwindcss'],
     format: ['cjs', 'esm'],
     minify: !options.watch,
     sourcemap: true,
