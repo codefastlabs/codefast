dc1873898b55f527bf08433e11c5c8881509d129
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-07-17 20:51:40 +0700

    Remove multiple plugin files and hooks
    The animate.plugin.ts, base.plugin.ts, perspective.plugin.ts, use-media-query.ts, use-mutation-observer.ts, and colors.ts files have been deleted. This was carried out as a part of larger codebase cleanup and restructuring process. The changes do not affect functionality but help reduce unnecessary complexity.

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -14,17 +14,6 @@ export default defineConfig((options) => [
   },
   {
     clean: !options.watch,
-    dts: true,
-    entry: ['tailwind.config.ts', 'plugin/**/*.ts'],
-    external: ['tailwindcss'],
-    format: ['cjs', 'esm'],
-    minify: !options.watch,
-    sourcemap: true,
-    splitting: true,
-    ...options,
-  },
-  {
-    clean: !options.watch,
     entry: ['src/**/*.css'],
     minify: !options.watch,
     sourcemap: true,
