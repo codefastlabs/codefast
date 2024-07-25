e7d911ad6425d3796e3bd7b1d3489091af4036f3
Vuong Phan <mr.thevuong@gmail.com>
2024-07-25 15:30:51 +0700

    Refactor input components and adjust configs
    Renamed and refactored TextInput and related components for better naming consistency and organization under a primitive directory. Updated Tailwind config spacing and ring colors, and adjusted import paths accordingly.

--- packages/config-tailwind/src/tailwind.config.ts
+++ packages/config-tailwind/src/tailwind.config.ts
@@ -113,6 +113,10 @@ const tailwindcssConfig: Omit<Config, 'content'> = {
       },
       spacing: {
         0.75: '0.1875rem', // 3px
+        1.25: '0.3125rem', // 5px
+        1.75: '0.4375rem', // 7px
+        2.25: '0.5625rem', // 9px
+        2.75: '0.6875rem', // 11px
         18: '4.5rem', // 72px
       },
       transitionDuration: {
