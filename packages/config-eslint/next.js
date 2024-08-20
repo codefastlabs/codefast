876beb907097baa2d0e589192f5aea41ba452c6f
Vuong Phan <mr.thevuong@gmail.com>
2024-08-20 09:07:14 +0700

    Update ESLint config order in next.js file
    Reorder the extends array in the ESLint configuration to prioritize the 'node' rules first, followed by 'typescript', 'browser', 'react', and 'next' rules. This change ensures that the configuration is applied appropriately, especially for environments where Node.js is used.

--- packages/config-eslint/next.js
+++ packages/config-eslint/next.js
@@ -5,8 +5,9 @@ const project = resolve(process.cwd(), 'tsconfig.json');
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [
-    require.resolve('@vercel/style-guide/eslint/browser'),
+    require.resolve('@vercel/style-guide/eslint/node'),
     require.resolve('@vercel/style-guide/eslint/typescript'),
+    require.resolve('@vercel/style-guide/eslint/browser'),
     require.resolve('@vercel/style-guide/eslint/react'),
     require.resolve('@vercel/style-guide/eslint/next'),
     require.resolve('./rules/common'),
