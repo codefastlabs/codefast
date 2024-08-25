610cd8d10b7d1a343a4e498fb47f0199776afeac
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-08-25 21:51:30 +0700

    docs(hooks): add TSDoc comments to use hooks
    Added detailed JSDoc comments to `useMutationObserver` and `useMediaQuery` functions. This improves the documentation by providing a clear description of the parameters, usage, and examples.

--- packages/hooks/src/use-mutation-observer.ts
+++ packages/hooks/src/use-mutation-observer.ts
@@ -1,5 +1,35 @@
 import * as React from 'react';
 
+/**
+ * Attaches a MutationObserver to a given HTMLElement and invokes a callback function when mutations occur in the
+ * observed elements.
+ *
+ * @param ref - The mutable reference to the HTMLElement to be observed for mutations.
+ * @param callback - The callback function to be invoked when mutations occur.
+ * @param options - The options object to configure the mutation observer. Defaults to observing attribute, character
+ *   data, child list changes, and subtree changes.
+ * @returns void
+ *
+ * @remarks
+ * This function is a React hook that attaches a MutationObserver to a given HTMLElement. The MutationObserver listens
+ *   for mutations in the observed element and its descendants and invokes the provided callback function when
+ *   mutations occur.
+ *
+ * @example
+ * // Referencing an HTMLElement in a React component
+ * const myRef = React.useRef<HTMLDivElement>(null);
+ *
+ * // Define the callback function to be invoked when mutations occur
+ * const handleMutations = (mutationsList: MutationRecord[], observer: MutationObserver) => {
+ *   // Process the mutations
+ *   // ...
+ * }
+ *
+ * // Attach the MutationObserver to the HTMLElement
+ * useMutationObserver(myRef, handleMutations);
+ *
+ * @see [MutationObserver](https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver)
+ */
 export function useMutationObserver(
   ref: React.MutableRefObject<HTMLElement | null>,
   callback: MutationCallback,
