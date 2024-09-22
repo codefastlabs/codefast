80fe9511249f4fd5e89ab0015b1865b8e7a7135f
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-09-22 16:04:39 +0700

    chore: update Separator components to adjust margin
    Changed the horizontal margin of Separator components in menubar, select, context-menu, and dropdown-menu from `-mx-1` to `mx-2`. This adjustment improves the visual spacing and consistency across the UI.

--- packages/ui/src/components/dropdown-menu.tsx
+++ packages/ui/src/components/dropdown-menu.tsx
@@ -243,7 +243,7 @@ const DropdownMenuSeparator = React.forwardRef<DropdownMenuSeparatorElement, Dro
   ({ className, ...props }, forwardedRef) => (
     <DropdownMenuPrimitive.Separator
       ref={forwardedRef}
-      className={cn('bg-muted -mx-1 my-1 h-px', className)}
+      className={cn('bg-muted mx-2 my-1 h-px', className)}
       {...props}
     />
   ),
