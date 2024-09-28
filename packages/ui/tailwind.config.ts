bba32789365381e7b9b118471f0466e4d8ea009c
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-09-28 09:19:26 +0700

    refactor: streamline tailwind config export
    Move tailwind config to default export from index.ts and update package.json configurations accordingly. Adjustments include proper type mappings and dependencies to reflect the new default export path.

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,5 +1,5 @@
 import { type Config } from 'tailwindcss';
-import sharedConfig from '@codefast/config-tailwind/tailwind.config';
+import sharedConfig from '@codefast/config-tailwind';
 
 const tailwindcssConfig: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
