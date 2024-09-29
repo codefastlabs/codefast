71e7a9bc597bbe179dd1be0736f2b4ffcd6bf8d4
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-09-29 22:56:48 +0700

    chore: update client libraries order in tsup config
    Reorganized the client libraries list in tsup.config.ts to align with internal use of the "use client" directive. Added comments to explain the context and rationale behind the order.

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'tsup';
 import { addUseClientDirective } from '@/add-use-client-directive';
 
+// Array of libraries used to detect client components in chunk files named "chunk-*".
+// This list is checked to determine if the "use client" directive should be added.
+// Notably, the @radix-ui/<package> libraries already include the "use client" directive internally,
+// so they are not included here.
+// However, there may be exceptions.
 const clientLibs = [
   '@radix-ui/react-context',
-  'vaul',
-  'cmdk',
-  'input-otp',
-  'react-resizable-panels',
   '@radix-ui/primitive',
+  'react-resizable-panels',
+  'input-otp',
+  'cmdk',
+  'vaul',
 ];
 
 export default defineConfig((options) => ({
