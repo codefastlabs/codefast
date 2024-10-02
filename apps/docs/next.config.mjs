a01ccc1c425cec970c65ea78c26b599bda2699f2
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-10-02 22:06:28 +0700

    chore: update Next.js and dependencies to canary version
    Updated Next.js and associated packages to the latest canary versions. This includes changes in pnpm-lock.yaml and modifying the Mail Page component to use async cookies.

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type { import("next").NextConfig } */
 const nextConfig = {
-  experimental: {
-    optimizePackageImports: ['@codefast/ui'],
-  },
+  // experimental: { optimizePackageImports: ['@codefast/ui'] },
   images: {
     remotePatterns: [
       {
