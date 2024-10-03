19dc1be20332674383bf00b5a7c74035c547985b
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-10-03 21:14:41 +0700

    chore: update dependencies in pnpm-lock.yaml
    Updated various dependencies including '@esbuild' versions and switched from lodash to lodash-es. Adjusted 'tsup' and 'jiti' versions across the lock file to maintain compatibility.

---
+++ apps/docs/next.config.ts
@@ -0,0 +1,8 @@
+import { type NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  // experimental: { optimizePackageImports: ['@codefast/ui'] },
+  images: { remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }] },
+};
+
+export default nextConfig;
