46dbec74075b2d5d5be8b0e17a3c07b25db47c95
Vuong Phan <mr.thevuong@gmail.com>
2024-10-04 13:07:06 +0700

    feat: replace TextInput with PasswordInput components
    Replaced TextInput with PasswordInput for better support of password-specific attributes. Included CSS styling for autofill and selection to improve user experience in dark mode.

--- apps/docs/stories/navigation/tabs.stories.tsx
+++ apps/docs/stories/navigation/tabs.stories.tsx
@@ -7,6 +7,7 @@ import {
   CardHeader,
   CardTitle,
   Label,
+  PasswordInput,
   Tabs,
   TabsContent,
   TabsList,
@@ -61,16 +62,16 @@ export const Default: Story = {
         <Card>
           <CardHeader>
             <CardTitle>Password</CardTitle>
-            <CardDescription>Change your password here. After saving, you'll be logged out.</CardDescription>
+            <CardDescription>Change your password here. After saving, you&#39;ll be logged out.</CardDescription>
           </CardHeader>
           <CardContent className="space-y-2">
             <div className="space-y-1">
               <Label htmlFor="current">Current password</Label>
-              <TextInput id="current" type="password" />
+              <PasswordInput id="current" />
             </div>
             <div className="space-y-1">
               <Label htmlFor="new">New password</Label>
-              <TextInput id="new" type="password" />
+              <PasswordInput id="new" />
             </div>
           </CardContent>
           <CardFooter>
