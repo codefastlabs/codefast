b313ac142777c7ea36e5e6a46cca3a010c46f4cf
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-10-04 21:26:20 +0700

    feat: add PasswordInput story to docs
    This commit introduces several Storybook stories for the PasswordInput component, showcasing its various states and configurations such as loading, prefix and suffix usage, different sizes, and controlled state. Additionally, updated the component code to improve UX by adding a callback for toggling password visibility and some minor style adjustments in input variants.

--- packages/ui/src/components/password-input.tsx
+++ packages/ui/src/components/password-input.tsx
@@ -27,6 +27,10 @@ const PasswordInput = React.forwardRef<PasswordInputElement, PasswordInputProps>
   ({ className, inputSize, loaderPosition = 'prefix', loading, prefix, spinner, suffix, ...props }, forwardedRef) => {
     const [type, setType] = React.useState<'password' | 'text'>('password');
 
+    const togglePasswordVisibility = React.useCallback<React.MouseEventHandler<HTMLButtonElement>>(() => {
+      setType((prev) => (prev === 'password' ? 'text' : 'password'));
+    }, []);
+
     return (
       <InputPrimitive.Root
         className={root({ inputSize, className })}
@@ -36,15 +40,21 @@ const PasswordInput = React.forwardRef<PasswordInputElement, PasswordInputProps>
         spinner={spinner || <Spinner />}
         suffix={suffix}
       >
-        <InputPrimitive.Item ref={forwardedRef} className={input({ inputSize })} type={type} {...props} />
-        <div className="order-last">
+        <InputPrimitive.Item
+          ref={forwardedRef}
+          className={input({ inputSize, className: 'w-full' })}
+          type={type}
+          {...props}
+        />
+        <div className="order-last aspect-square h-full p-0.5">
           <Button
             icon
-            size="xs"
+            aria-label={type === 'password' ? 'Show password' : 'Hide password'}
+            className="size-full"
+            disabled={props.disabled}
+            size={inputSize}
             variant="ghost"
-            onClick={() => {
-              setType((prev) => (prev === 'password' ? 'text' : 'password'));
-            }}
+            onClick={togglePasswordVisibility}
           >
             {type === 'password' ? <EyeClosedIcon /> : <EyeOpenIcon />}
           </Button>
