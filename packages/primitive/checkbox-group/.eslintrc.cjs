cb8b5af5ba8eba376add44c3263a91241d5335e9
BM Valentino <valentino@brickmate.vn>
2024-10-10 10:33:17 +0700

    refactor: replace .eslintrc.cjs with .eslintrc.json
    Switched ESLint configuration files from .cjs to .json format across multiple packages. This change improves consistency and readability of configuration settings.

--- packages/primitive/checkbox-group/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-/** @type { import("eslint").Linter.Config } */
-module.exports = {
-  extends: ['@codefast/eslint-config/react'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-  },
-  root: true,
-};
