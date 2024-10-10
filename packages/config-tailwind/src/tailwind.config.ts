46d86c6f1b163456870fbe432e3c1cf3f0830dc7
BM Valentino <valentino@brickmate.vn>
2024-10-10 12:04:21 +0700

    refactor: reorder import statement in tailwind.config.ts
    Moved the `fontFamily` import after `Config` for better readability and consistency. This change does not affect functionality but improves code organization.

--- packages/config-tailwind/src/tailwind.config.ts
+++ packages/config-tailwind/src/tailwind.config.ts
@@ -1,5 +1,5 @@
-import { fontFamily } from 'tailwindcss/defaultTheme';
 import { type Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 import animate from '@/plugin/animate.plugin';
 import base from '@/plugin/base.plugin';
 import perspective from '@/plugin/perspective.plugin';
