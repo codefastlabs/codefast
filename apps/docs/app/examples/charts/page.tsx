750e051116a48b9c7baf9d67ab73d5424763c501
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-10-11 17:57:10 +0700

    feat(charts): add area chart examples and resources
    Introduced two new area chart components, ChartAreaAxes and ChartAreaDefault, in the documentation. Added necessary chart colors to global CSS. Updated examples navigation to include the new charts section.

---
+++ apps/docs/app/examples/charts/page.tsx
@@ -0,0 +1,19 @@
+import { type Metadata } from 'next';
+import { type JSX } from 'react';
+import { ChartAreaAxes } from '@/app/examples/charts/_components/chart-area-axes';
+import { ChartAreaDefault } from '@/app/examples/charts/_components/chart-area-default';
+
+export const metadata: Metadata = {
+  title: 'Charts',
+};
+
+export default function ChartsPage(): JSX.Element {
+  return (
+    <main className="grid gap-4">
+      <div className="grid flex-1 scroll-mt-20 items-start gap-10 md:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:gap-10">
+        <ChartAreaDefault />
+        <ChartAreaAxes />
+      </div>
+    </main>
+  );
+}
