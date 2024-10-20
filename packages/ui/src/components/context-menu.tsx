d6d54dc1f25faf989930a7c44f5f2128ca60b91b
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-10-20 09:29:58 +0700

    refactor: standardize border radius across components
    Replaced 'rounded-sm' class with 'rounded' class in various UI components. This change ensures consistency in border radius styling throughout the application.

--- packages/ui/src/components/context-menu.tsx
+++ packages/ui/src/components/context-menu.tsx
@@ -77,7 +77,7 @@ const ContextMenuSubTrigger = forwardRef<
     ref={forwardedRef}
     className={cn(
       'gap-x-2 px-3 py-1.5',
-      'flex cursor-pointer select-none items-center rounded-sm text-sm',
+      'flex cursor-pointer select-none items-center rounded text-sm',
       'focus:bg-accent focus:text-accent-foreground focus:outline-none',
       'data-[state=open]:bg-accent data-[state=open]:text-accent-foreground',
       inset && 'pl-8',
@@ -184,7 +184,7 @@ const ContextMenuItem = forwardRef<
     ref={forwardedRef}
     className={cn(
       'gap-x-2 px-3 py-1.5',
-      'relative flex cursor-pointer select-none items-center rounded-sm text-sm',
+      'relative flex cursor-pointer select-none items-center rounded text-sm',
       'focus:bg-accent focus:text-accent-foreground focus:outline-none',
       'aria-disabled:pointer-events-none aria-disabled:opacity-50',
       inset && 'pl-8',
@@ -217,7 +217,7 @@ const ContextMenuCheckboxItem = forwardRef<
     className={cn(
       'gap-x-2 px-3 py-1.5',
       'pl-8',
-      'relative flex cursor-pointer select-none items-center rounded-sm text-sm',
+      'relative flex cursor-pointer select-none items-center rounded text-sm',
       'focus:bg-accent focus:text-accent-foreground focus:outline-none',
       'aria-disabled:pointer-events-none aria-disabled:opacity-50',
       className,
@@ -256,7 +256,7 @@ const ContextMenuRadioItem = forwardRef<
     className={cn(
       'gap-x-2 px-3 py-1.5',
       'pl-8',
-      'relative flex cursor-pointer select-none items-center rounded-sm text-sm',
+      'relative flex cursor-pointer select-none items-center rounded text-sm',
       'focus:bg-accent focus:text-accent-foreground focus:outline-none',
       'aria-disabled:pointer-events-none aria-disabled:opacity-50',
       className,
