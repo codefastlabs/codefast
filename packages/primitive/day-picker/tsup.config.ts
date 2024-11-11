85195fd775011ca1113b47ec028f1f4375d60517
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-11-11 22:37:49 +0700

    feat(ui): add new Sidebar component
    Introduce Sidebar component along with associated sub-components in the UI package. This includes features like collapsible sidebar, keyboard shortcut for toggling, and various styling options to enhance user experience.

---
+++ packages/primitive/day-picker/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig((options) => ({
+  clean: !options.watch,
+  dts: true,
+  entry: ['src/**/*.ts*'],
+  format: ['cjs', 'esm'],
+  minify: !options.watch,
+  shims: true,
+  silent: true,
+  sourcemap: true,
+  splitting: true,
+  ...options,
+}));
