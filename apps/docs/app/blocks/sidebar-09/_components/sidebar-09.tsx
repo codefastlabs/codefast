e075f094ff5f6eb9df5c21a70423eea1493a7e3a
BM Valentino <valentino@brickmate.vn>
2024-11-11 14:26:11 +0700

    feat: add Sidebar 09 component with its dependencies
    Introduced a new Sidebar 09 component along with associated dependencies, including `AppSidebar` and `NavUser`. Refactored placeholder grid and mock data to be shared across different sidebar components.

---
+++ apps/docs/app/blocks/sidebar-09/_components/sidebar-09.tsx
@@ -0,0 +1,53 @@
+import {
+  cn,
+  Separator,
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@codefast/ui';
+import { type ComponentProps, type JSX } from 'react';
+
+import { PlaceholderGrid } from '@/app/blocks/_components/placeholder-grid';
+import { AppSidebar } from '@/app/blocks/sidebar-09/_components/app-sidebar';
+
+type Sidebar09Props = ComponentProps<'div'>;
+
+export function Sidebar09({ className, ...props }: Sidebar09Props): JSX.Element {
+  return (
+    <div className={cn('', className)} {...props}>
+      <SidebarProvider
+        style={
+          {
+            '--sidebar-width': '350px',
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar />
+        <SidebarInset>
+          <header className="bg-background sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator className="mr-2 h-4" orientation="vertical" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">All Inboxes</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Inbox</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </header>
+          <PlaceholderGrid className="flex flex-1 flex-col gap-4 p-4" />
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+  );
+}
---
+++ apps/docs/app/blocks/sidebar-09/page.tsx
@@ -0,0 +1,16 @@
+import { type Metadata } from 'next';
+import { type JSX } from 'react';
+
+import { Sidebar09 } from '@/app/blocks/sidebar-09/_components/sidebar-09';
+
+export const metadata: Metadata = {
+  title: 'Sidebar 09',
+};
+
+export default function Sidebar09Page(): JSX.Element {
+  return (
+    <main className="">
+      <Sidebar09 />
+    </main>
+  );
+}
