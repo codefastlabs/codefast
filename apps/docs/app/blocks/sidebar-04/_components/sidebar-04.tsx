5b48e975c86326d7e0778b0907e539e3fcc839ab
BM Valentino <valentino@brickmate.vn>
2024-11-11 11:58:22 +0700

    feat: add Sidebar 04 example
    Introduced a new "Sidebar 04" example component and its associated page. Updated the sidebars list to include this new example. The component incorporates elements like SidebarMenu, SidebarGroup, and breadcrumb navigation.

---
+++ apps/docs/app/blocks/sidebar-04/_components/sidebar-04.tsx
@@ -0,0 +1,115 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  cn,
+  Separator,
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@codefast/ui';
+import { GalleryVerticalEndIcon } from 'lucide-react';
+import Link from 'next/link';
+import { type ComponentProps, type JSX } from 'react';
+
+import { data } from '@/app/blocks/sidebar-04/_lib/mocks/data';
+
+type Sidebar04Props = ComponentProps<'div'>;
+
+export function Sidebar04({ className, ...props }: Sidebar04Props): JSX.Element {
+  return (
+    <div className={cn('', className)} {...props}>
+      <SidebarProvider
+        style={
+          {
+            '--sidebar-width': '19rem',
+          } as React.CSSProperties
+        }
+      >
+        <Sidebar variant="floating">
+          <SidebarHeader>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild size="lg">
+                  <Link href="#">
+                    <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                      <GalleryVerticalEndIcon className="size-4" />
+                    </div>
+                    <div className="flex flex-col gap-0.5 leading-none">
+                      <span className="font-semibold">Documentation</span>
+                      <span className="">v1.0.0</span>
+                    </div>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarHeader>
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarMenu className="gap-2">
+                {data.navMain.map((nav) => (
+                  <SidebarMenuItem key={nav.title}>
+                    <SidebarMenuButton asChild>
+                      <Link className="font-medium" href={nav.url}>
+                        {nav.title}
+                      </Link>
+                    </SidebarMenuButton>
+                    {nav.items.length ? (
+                      <SidebarMenuSub className="ml-0 border-l-0 px-1.5">
+                        {nav.items.map((item) => (
+                          <SidebarMenuSubItem key={item.title}>
+                            <SidebarMenuSubButton asChild isActive={item.isActive}>
+                              <Link href={item.url}>{item.title}</Link>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    ) : null}
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroup>
+          </SidebarContent>
+        </Sidebar>
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator className="mr-2 h-4" orientation="vertical" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </header>
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+              <div className="bg-muted/50 aspect-video rounded-xl" />
+              <div className="bg-muted/50 aspect-video rounded-xl" />
+              <div className="bg-muted/50 aspect-video rounded-xl" />
+            </div>
+            <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+  );
+}
