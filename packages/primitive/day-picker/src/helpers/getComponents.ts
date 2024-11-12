d3b861277d3cc4ecfcfe94bd1869ef3c2fd5b8a1
BM Valentino <valentino@brickmate.vn>
2024-11-12 09:53:57 +0700

    feat(day-picker): add ARIA labels and selection hooks
    Introduce ARIA labels and localization support for day-picker components, covering elements like week numbers, dropdowns, and buttons. Add `useMulti`, `useRange`, and `useSingle` hooks to manage different selection modes effectively.

---
+++ packages/primitive/day-picker/src/helpers/getComponents.ts
@@ -0,0 +1,9 @@
+import * as components from '../components/custom-components.js';
+import type { CustomComponents, DayPickerProps } from '../types/index.js';
+
+export function getComponents(customComponents: DayPickerProps['components']): CustomComponents {
+  return {
+    ...components,
+    ...customComponents,
+  };
+}
