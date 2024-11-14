6838bf7e2e99626605304f0f8672de536a2e965f
BM Valentino <valentino@brickmate.vn>
2024-11-14 16:00:21 +0700

    feat(day-picker): add modifiers styling example with tests
    Introduced a new example for styling day modifiers in the DayPicker component. Added tests to ensure styles are applied correctly to specified dates and verified that other dates do not receive the styles.

---
+++ packages/primitive/day-picker/examples/modifiers-style.test.tsx
@@ -0,0 +1,37 @@
+import { render } from '@testing-library/react';
+
+import { gridcell } from './lib/elements';
+import { availableDays, ModifiersStyle } from './modifiers-style';
+
+const style = {
+  fontWeight: 900,
+  color: 'lightgreen',
+};
+
+const today = new Date(2021, 10, 25);
+
+beforeAll(() => {
+  jest.setSystemTime(today);
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
+
+describe('modifiers-style component', () => {
+  test('should render without crashing', () => {
+    const { container } = render(<ModifiersStyle />);
+
+    expect(container).toBeInTheDocument();
+  });
+
+  test.each(availableDays)('applies available style to day %s', (day) => {
+    render(<ModifiersStyle />);
+    expect(gridcell(day, true)).toHaveStyle(style);
+  });
+
+  test('does not apply available style to a day not in availableDays', () => {
+    render(<ModifiersStyle />);
+    expect(gridcell(today, true)).not.toHaveStyle(style);
+  });
+});
