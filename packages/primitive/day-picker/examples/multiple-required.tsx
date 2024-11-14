e7303bd4d7336906c53bbd264e1e66ab323029d8
BM Valentino <valentino@brickmate.vn>
2024-11-14 17:44:00 +0700

    feat(day-picker): add multiple-required example and tests
    Introduce an example for the multiple-required mode in DayPicker component. Also, add tests to ensure that the required attribute is obeyed, allowing selection of multiple dates but preventing unselecting the last selected date.

---
+++ packages/primitive/day-picker/examples/multiple-required.tsx
@@ -0,0 +1,7 @@
+import { type JSX } from 'react';
+
+import { DayPicker } from '@/components';
+
+export function MultipleRequired(): JSX.Element {
+  return <DayPicker required mode="multiple" selected={[new Date()]} />;
+}
