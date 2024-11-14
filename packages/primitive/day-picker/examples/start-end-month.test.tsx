64ca63ded0f99ad32e35c4b1a3016f553f02fa32
BM Valentino <valentino@brickmate.vn>
2024-11-14 13:34:23 +0700

    feat(day-picker): add start-end-month example and tests
    Introduced a new example component for DayPicker that demonstrates usage with start and end months. Added tests to verify button states at start and end months.

---
+++ packages/primitive/day-picker/examples/start-end-month.test.tsx
@@ -0,0 +1,40 @@
+import { render } from '@testing-library/react';
+import { differenceInMonths } from 'date-fns';
+
+import { nextButton, previousButton } from './lib/elements';
+import { user } from './lib/user';
+import { StartEndMonth } from './start-end-month';
+
+describe('start-end-month component', () => {
+  beforeEach(() => {
+    render(<StartEndMonth />);
+  });
+
+  test('disables the previous button on the start month', () => {
+    expect(previousButton()).toHaveAttribute('disabled');
+  });
+
+  test('enables the next button on the start month', () => {
+    expect(nextButton()).not.toHaveAttribute('disabled');
+  });
+
+  describe('when navigating to the end month', () => {
+    const fromDate = new Date(2015, 5);
+    const toDate = new Date(2015, 10);
+    const numberOfMonths = differenceInMonths(toDate, fromDate);
+
+    beforeEach(async () => {
+      for (let i = 0; i < numberOfMonths; i++) {
+        await user.click(nextButton());
+      }
+    });
+
+    test('enables the previous button on the end month', () => {
+      expect(previousButton()).not.toHaveAttribute('disabled');
+    });
+
+    test('disables the next button on the end month', () => {
+      expect(nextButton()).toHaveAttribute('disabled');
+    });
+  });
+});
