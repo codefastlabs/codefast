0661de1cfd5b2b68a1b700de9d3dd56d43b67979
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-11-14 11:59:54 +0700

    feat(day-picker): add custom month caption formatting
    Introduce a new example and test for custom month caption formatting in the day picker. This includes seasonal emojis and ensures various seasons display correct captions using date-fns for formatting.

---
+++ packages/primitive/day-picker/examples/format-caption.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen } from '@testing-library/react';
+
+import { FormatCaption } from './format-caption';
+
+const today = new Date(2021, 10, 25); // 25 November 2021
+
+beforeAll(() => {
+  jest.setSystemTime(today);
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
+
+describe('format-caption component', () => {
+  it('renders without crashing', () => {
+    const { container } = render(<FormatCaption />);
+
+    expect(container).toBeInTheDocument();
+  });
+
+  it('displays the correct season emoji and month name for autumn', () => {
+    render(<FormatCaption />);
+    expect(screen.getByText('🍂 November')).toBeInTheDocument();
+  });
+
+  it('displays the correct season emoji and month name for winter', () => {
+    const winterMonth = new Date(2022, 1, 1); // February 2022
+
+    jest.setSystemTime(winterMonth);
+
+    render(<FormatCaption />);
+
+    expect(screen.getByText('⛄️ February')).toBeInTheDocument();
+  });
+
+  it('displays the correct season emoji and month name for spring', () => {
+    const springMonth = new Date(2022, 4, 1); // May 2022
+
+    jest.setSystemTime(springMonth);
+
+    render(<FormatCaption />);
+
+    expect(screen.getByText('🌸 May')).toBeInTheDocument();
+  });
+
+  it('displays the correct season emoji and month name for summer', () => {
+    const summerMonth = new Date(2022, 7, 1); // August 2022
+
+    jest.setSystemTime(summerMonth);
+
+    render(<FormatCaption />);
+
+    expect(screen.getByText('🌻 August')).toBeInTheDocument();
+  });
+});
