d7303ed2227f16cd6cdce5247b143859820628ef
BM Valentino <valentino@brickmate.vn>
2024-11-14 17:44:00 +0700

    feat(day-picker): add multiple-required example and tests
    Introduce an example for the multiple-required mode in DayPicker component. Also, add tests to ensure that the required attribute is obeyed, allowing selection of multiple dates but preventing unselecting the last selected date.

---
+++ packages/primitive/day-picker/examples/multiple-required.test.tsx
@@ -0,0 +1,49 @@
+import { fireEvent, render } from '@testing-library/react';
+import { addDays } from 'date-fns';
+
+import { dateButton, gridcell } from './lib/elements';
+import { MultipleRequired } from './multiple-required';
+
+const today = new Date();
+const anotherDay = addDays(today, 1);
+
+beforeAll(() => {
+  jest.setSystemTime(today);
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
+
+describe('multiple-required component', () => {
+  beforeEach(() => {
+    render(<MultipleRequired />);
+  });
+
+  test('initially displays today as selected', () => {
+    expect(gridcell(today, true)).toHaveAttribute('aria-selected', 'true');
+  });
+
+  test('does not allow unselecting the only selected day', () => {
+    fireEvent.click(dateButton(today));
+    expect(gridcell(today, true)).toHaveAttribute('aria-selected', 'true');
+  });
+
+  describe('when another day is selected', () => {
+    beforeEach(() => {
+      fireEvent.click(dateButton(anotherDay));
+    });
+
+    test('both today and the other day should be selected', () => {
+      expect(gridcell(today, true)).toHaveAttribute('aria-selected', 'true');
+      expect(gridcell(anotherDay, true)).toHaveAttribute('aria-selected', 'true');
+    });
+
+    test('allows unselecting one day if more than one day is selected', () => {
+      fireEvent.click(dateButton(today));
+
+      expect(gridcell(today, true)).not.toHaveAttribute('aria-selected');
+      expect(gridcell(anotherDay, true)).toHaveAttribute('aria-selected', 'true');
+    });
+  });
+});
