200fff202a637e9612be1cd70cd0061640c5d8e7
BM Valentino <valentino@brickmate.vn>
2024-11-14 13:51:40 +0700

    test(input): add unit tests for Input component
    Introduce unit tests for Input component to verify the correct date selection and input behavior. Tests include validation for valid/invalid dates, clearing input, and updating the calendar accordingly.

---
+++ packages/primitive/day-picker/examples/input.tsx
@@ -0,0 +1,77 @@
+import { format, isValid, parse } from 'date-fns';
+import { type JSX, useId, useState } from 'react';
+
+import { DayPicker } from '@/components';
+import { type OnSelectHandler } from '@/lib';
+
+export function Input(): JSX.Element {
+  const inputId = useId();
+
+  // Hold the month in state to control the calendar when the input changes
+  const [month, setMonth] = useState(new Date());
+
+  // Hold the selected date in state
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined);
+
+  // Hold the input value in state
+  const [inputValue, setInputValue] = useState('');
+
+  const handleDayPickerSelect: OnSelectHandler<Date | undefined> = (date) => {
+    if (!date) {
+      setInputValue('');
+      setSelectedDate(undefined);
+    } else {
+      setSelectedDate(date);
+      setMonth(date);
+      setInputValue(format(date, 'MM/dd/yyyy'));
+    }
+  };
+
+  /**
+   * A function to handle changes in an input field. It updates the input value and tries to parse it as a date in
+   * MM/dd/yyyy format. If the input value represents a valid date, it sets the selected date and updates the month.
+   * Otherwise, it clears the selected date.
+   *
+   * @param e - The change event triggered by the input element.
+   */
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(e.target.value); // keep the input value in sync
+
+    const parsedDate = parse(e.target.value, 'MM/dd/yyyy', new Date());
+
+    if (isValid(parsedDate)) {
+      setSelectedDate(parsedDate);
+      setMonth(parsedDate);
+    } else {
+      setSelectedDate(undefined);
+    }
+  };
+
+  return (
+    <div>
+      <label htmlFor={inputId}>
+        <strong>Date:</strong>
+      </label>
+
+      <input
+        id={inputId}
+        placeholder="MM/dd/yyyy"
+        style={{ fontSize: 'inherit', padding: '0.25em 0.5em' }}
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+
+      <div style={{ marginBlock: '1em' }}>
+        <DayPicker
+          footer={`Selected: ${selectedDate?.toDateString()}`}
+          mode="single"
+          month={month}
+          selected={selectedDate}
+          onMonthChange={setMonth}
+          onSelect={handleDayPickerSelect}
+        />
+      </div>
+    </div>
+  );
+}
