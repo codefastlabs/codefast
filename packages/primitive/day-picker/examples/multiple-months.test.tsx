cec92425633bfa53fec031769abf82ee8bf55ef7
BM Valentino <valentino@brickmate.vn>
2024-11-14 16:52:16 +0700

    feat(day-picker): add multiple months display and tests
    Introduce the `MultipleMonths` component to display two consecutive months in the `DayPicker`. Added tests to ensure the correctness of the grid elements and their names when navigating between months.

---
+++ packages/primitive/day-picker/examples/multiple-months.test.tsx
@@ -0,0 +1,47 @@
+import { render } from '@testing-library/react';
+
+import { grids, previousButton } from './lib/elements';
+import { user } from './lib/user';
+import { MultipleMonths } from './multiple-months';
+
+const today = new Date(2023, 11, 3);
+
+beforeAll(() => {
+  jest.setSystemTime(today);
+});
+
+afterAll(() => {
+  return jest.useRealTimers();
+});
+
+describe('multiple-months component', () => {
+  beforeEach(() => {
+    render(<MultipleMonths />);
+  });
+
+  test('renders 2 grids representing two consecutive months', () => {
+    expect(grids()).toHaveLength(2);
+  });
+
+  test('displays the current month in the first grid', () => {
+    expect(grids()[0]).toHaveAccessibleName('December 2023');
+  });
+
+  test('displays the next month in the second grid', () => {
+    expect(grids()[1]).toHaveAccessibleName('January 2024');
+  });
+
+  describe('when the previous month button is clicked', () => {
+    beforeEach(async () => {
+      await user.click(previousButton());
+    });
+
+    test('updates the first grid to show the previous month', () => {
+      expect(grids()[0]).toHaveAccessibleName('November 2023');
+    });
+
+    test('updates the second grid to show the original current month', () => {
+      expect(grids()[1]).toHaveAccessibleName('December 2023');
+    });
+  });
+});
