b2352d2d0a7ec1db93caa6bf5dc884e612568927
BM Valentino <valentino@brickmate.vn>
2024-11-15 11:55:57 +0700

    test: add unit tests for Single component in DayPicker
    Introduce unit tests to verify the functionality of the Single component within the DayPicker. The tests cover selection, focus, and the toggling of a selected day's state.

---
+++ packages/primitive/day-picker/examples/single.test.tsx
@@ -0,0 +1,52 @@
+import { render } from '@testing-library/react';
+
+import { dateButton, gridcell } from './lib/elements';
+import { user } from './lib/user';
+import { Single } from './single';
+
+const today = new Date(2021, 10, 25);
+
+beforeAll(() => {
+  jest.setSystemTime(today);
+});
+afterAll(() => jest.useRealTimers());
+
+describe('single component', () => {
+  beforeEach(() => {
+    render(<Single />);
+  });
+
+  describe('when a single day is clicked', () => {
+    const day = new Date(2021, 10, 1);
+
+    beforeEach(async () => {
+      await user.click(dateButton(day));
+    });
+
+    test('marks the day as selected', () => {
+      expect(gridcell(day, true)).toHaveAttribute('aria-selected', 'true');
+    });
+
+    test('focuses on the selected day', () => {
+      expect(dateButton(day)).toHaveFocus();
+    });
+
+    test('applies the selected class to the day', () => {
+      expect(gridcell(day, true)).toHaveClass('rdp-selected');
+    });
+
+    describe('when the selected day is clicked again', () => {
+      beforeEach(async () => {
+        await user.click(dateButton(day));
+      });
+
+      test('removes the selected state from the day', () => {
+        expect(gridcell(day, true)).not.toHaveAttribute('aria-selected');
+      });
+
+      test('removes the selected class from the day', () => {
+        expect(gridcell(day, true)).not.toHaveClass('rdp-selected');
+      });
+    });
+  });
+});
