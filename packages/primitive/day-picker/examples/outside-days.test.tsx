a8e533a107617e2d9ddc91f0680bca1b19b2bc9f
BM Valentino <valentino@brickmate.vn>
2024-11-15 09:21:26 +0700

    test(day-picker): add tests for outside days and refactor timers
    Introduced tests for the new `outside-days` component to ensure accurate day rendering. Refactored timer setup and teardown in existing tests to use `beforeAll` and `afterAll` hooks for consistency.

---
+++ packages/primitive/day-picker/examples/outside-days.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+
+import { labelGridcell } from '@/lib';
+
+import { gridcell } from './lib/elements';
+import { OutsideDays } from './outside-days';
+
+const today = new Date(2021, 10, 25);
+
+beforeAll(() => {
+  jest.setSystemTime(today);
+});
+
+afterAll(() => {
+  return jest.useRealTimers();
+});
+
+describe('outside-days component', () => {
+  beforeEach(() => {
+    render(<OutsideDays />);
+  });
+
+  describe('when displaying a month view with outside days enabled', () => {
+    test('displays days from the previous month', () => {
+      expect(gridcell(new Date(2021, 9, 31))).toBeInTheDocument(); // Ngày 31 tháng 10 là ngày ngoài tháng
+    });
+
+    test('displays today’s date within the month view', () => {
+      expect(
+        screen.getByRole('cell', {
+          name: labelGridcell(today, {
+            today: true,
+          }),
+        }),
+      ).toBeInTheDocument();
+    });
+  });
+});
