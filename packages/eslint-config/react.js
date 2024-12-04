22d2e4c9db2b66c48ab0b5a19c589f8aefe1747a
BM Valentino <valentino@brickmate.vn>
2024-12-04 17:58:02 +0700

    chore(eslint): remove individual ESLint config files
    Removed .eslintrc.json files from multiple directories, consolidating linting configuration. This change aims to streamline and centralize ESLint configurations for better maintenance. Additionally, upgraded ESLint to version 9.16.0 across dependencies for consistency and to leverage the latest features.

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -1,73 +1,99 @@
-const { resolve } = require('node:path');
+import { config as codefast } from '@codefast/style-guide';
+
+import { resolve } from 'node:path';
 
 const project = resolve(process.cwd(), 'tsconfig.json');
 
-/** @type { import('eslint').Linter.Config } */
-module.exports = {
-  extends: [
-    require.resolve('@vercel/style-guide/eslint/browser'),
-    require.resolve('@vercel/style-guide/eslint/typescript'),
-    require.resolve('@vercel/style-guide/eslint/react'),
-    require.resolve('./eslint/recommended'),
-  ],
-  globals: {
-    JSX: true,
-    NodeJS: true,
-    jest: true,
+/**
+ * @type {import('eslint').Linter.Config[]}
+ */
+export const config = [
+  ...codefast.configs.recommended,
+  ...codefast.configs.react,
+  {
+    ignores: ['dist/', 'coverage/'],
   },
-  ignorePatterns: ['node_modules/', 'dist/', '*.js', '*.mjs', '*.cjs'],
-  overrides: [
-    {
-      extends: [require.resolve('@vercel/style-guide/eslint/jest-react')],
-      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(test).[jt]s?(x)'],
+  /* {
+    extends: [
+      require.resolve('@vercel/style-guide/eslint/browser'),
+      require.resolve('@vercel/style-guide/eslint/typescript'),
+      require.resolve('@vercel/style-guide/eslint/react'),
+      require.resolve('./eslint/recommended'),
+    ],
+    globals: {
+      JSX: true,
+      NodeJS: true,
+      jest: true,
     },
-    {
-      extends: [require.resolve('@vercel/style-guide/eslint/playwright-test')],
-      files: ['**/e2e/**/*.[jt]s?(x)', '**/?(*.)+(spec|e2e).[jt]s?(x)'],
-    },
-  ],
-  parserOptions: {
-    project,
-  },
-  rules: {
-    /**
-     * Warns when JSX components aren't in PascalCase but allows namespaces
-     * TODO: Remove after upgrading the style guide to my own version.
-     */
-    'react/jsx-pascal-case': [
-      'warn',
+    ignorePatterns: ['node_modules/', 'dist/', '*.js', '*.mjs', '*.cjs'],
+    overrides: [
       {
-        allowNamespace: true,
+        extends: [require.resolve('@vercel/style-guide/eslint/jest-react')],
+        files: ['**!/__tests__/!**!/!*.[jt]s?(x)', '**!/?(*.)+(test).[jt]s?(x)'],
+      },
+      {
+        extends: [require.resolve('@vercel/style-guide/eslint/playwright-test')],
+        files: ['**!/e2e/!**!/!*.[jt]s?(x)', '**!/?(*.)+(spec|e2e).[jt]s?(x)'],
       },
     ],
+    parserOptions: {
+      project,
+    },
+  }, */
+  {
+    rules: {
+      /**
+       * Disables the rule that disallows default exports
+       */
+      'import/no-default-export': 'off',
 
-    /**
-     * Enforces sorting of JSX props
-     * TODO: Remove after upgrading the style guide to my own version.
-     */
-    'react/jsx-sort-props': [
-      'warn',
-      {
-        callbacksLast: true,
-        ignoreCase: true,
-        reservedFirst: true,
-        shorthandFirst: true,
-      },
-    ],
+      /**
 
-    /** Warns when using unknown DOM properties but ignores specified custom elements */
-    'react/no-unknown-property': [
-      'warn',
-      {
-        ignore: ['cmdk-input-wrapper'],
-      },
-    ],
+       * Warns when JSX components aren't in PascalCase but allows namespac
+       es
+       * TODO: Remove after upgrading the style guide to my own version.
+       */
+      'react/jsx-pascal-case': [
+        'warn',
+        {
+          allowNamespace: true,
+        },
+      ],
+
+      /**
+
+       * Enforces sorting of JSX pro
+       ps
+       * TODO: Remove after upgrading the style guide to my own version.
+       */
+      'react/jsx-sort-props': [
+        'warn',
+        {
+          callbacksLast: true,
+          ignoreCase: true,
+          reservedFirst: true,
+          shorthandFirst: true,
+        },
+      ],
+
+      /**
+       * Warns when using unknown DOM properties but ignores specified custom elements
+       */
+      'react/no-unknown-property': [
+        'warn',
+        {
+          ignore: ['cmdk-input-wrapper'],
+        },
+      ],
+    },
   },
-  settings: {
-    'import/resolver': {
-      typescript: {
-        project,
+  {
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project,
+        },
       },
     },
   },
-};
+];
