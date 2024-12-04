7e0e758caff294358e67f12b53df353609f81afa
BM Valentino <valentino@brickmate.vn>
2024-12-04 09:01:46 +0700

    refactor(style-guide): restructure style guide and rules
    Deleted `index.js` and introduced distinct rule files for better organization. Rules are now sorted into categories such as stylistic, ES6, and React, providing clearer guidance and modular rule management. This restructuring enhances readability and facilitates future updates to the style guide.

---
+++ packages/style-guide/src/rules/comments.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    /**
+     * Require comments on ESlint disable directives.
+     *
+     * 🚫 Not fixable - https://eslint-community.github.io/eslint-plugin-eslint-comments/rules/require-description.html
+     */
+    '@eslint-community/eslint-comments/require-description': 'error',
+  },
+};
