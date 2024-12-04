2b9b1b805db8fe2cb2ef0915692abccd29cfd632
BM Valentino <valentino@brickmate.vn>
2024-12-04 12:59:23 +0700

    feat(style-guide): add extensive ESLint configurations
    Introduce comprehensive ESLint configurations for various frameworks, libraries, and scenarios such as testing, TypeScript, and React. This includes importing necessary plugins and setting up rule configurations to enhance code quality and consistency across the project. Also, ensure peer dependencies are checked and provide necessary Babel options for Next.js integrations.

---
+++ packages/style-guide/src/lib/constants.js
@@ -0,0 +1,5 @@
+export const ECMA_VERSION = 2021;
+export const JAVASCRIPT_FILES = ['**/*.js?(x)', '**/*.(c|m)?js'];
+export const TYPESCRIPT_FILES = ['**/*.ts?(x)', '**/*.(c|m)?ts'];
+export const TEST_FILES = ['**/?(*.)+(test|spec).[jt]s?(x)'];
+export const TYPESCRIPT_TEST_FILES = ['**/?(*.)+(test|spec).ts?(x)'];
