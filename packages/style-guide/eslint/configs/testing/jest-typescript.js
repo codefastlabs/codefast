2142fe4f8dc404be10d09ca06c8511d18279f123
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-12-04 23:00:35 +0700

    refactor(eslint): remove Babel and file-specific configs
    This commit removes several Babel-related configurations and constants from the ESLint setup as well as file-specific configurations. This streamlines the ESLint configuration by reducing dependencies and simplifying the overall setup. Additionally, the change updates the structure to better align with a modern, streamlined ESLint configuration approach.

--- packages/style-guide/eslint/configs/testing/jest-typescript.js
+++ packages/style-guide/eslint/configs/testing/jest-typescript.js
@@ -1,8 +1,5 @@
-import { TYPESCRIPT_TEST_FILES } from '../../lib/constants.js';
-
 /** @type {import('eslint').Linter.Config} */
 export const config = {
-  files: TYPESCRIPT_TEST_FILES,
   rules: {
     '@typescript-eslint/unbound-method': 'off',
     'jest/unbound-method': 'error',
--- packages/style-guide/eslint/configs/testing/vitest.js
+++ packages/style-guide/eslint/configs/testing/vitest.js
@@ -1,11 +1,9 @@
 import vitest from '@vitest/eslint-plugin';
 
 import { config as vitestRules } from '../../rules/vitest.js';
-import { UNIT_TEST_FILES } from '../../lib/constants.js';
 
 /** @type {import('eslint').Linter.Config} */
 export const config = {
-  files: UNIT_TEST_FILES,
   plugins: {
     vitest,
   },
