64e25fcd91396fb8f32284288c0ee2a88d49a099
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-12-05 21:59:37 +0700

    feat: disable unnecessary type parameter rule in configs
    Turn off '@typescript-eslint/no-unnecessary-type-parameters' across eslint configurations for next, react, and library packages. This change allows developers to add explicit type parameters for consistency and future-proofing without enforcement to remove them if unnecessary.

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -81,6 +81,21 @@ export const config = [
       ],
 
       /**
+       * This configuration rule is part of the `@typescript-eslint` package.
+       *
+       * It disables the rule that enforces the removal of unnecessary type parameters in TypeScript.
+       *
+       * By turning off '`no-unnecessary-type-parameters`', developers can define type parameters in functions,
+       * classes, or interfaces even when those parameters are not strictly required for type inference or checking.
+       *
+       * This can be useful in scenarios where explicit type parameters are used for consistency,
+       * readability, or future-proofing code even if they are not technically required by the current implementation.
+       *
+       * https://typescript-eslint.io/rules/no-unnecessary-type-parameters/
+       */
+      '@typescript-eslint/no-unnecessary-type-parameters': 'off',
+
+      /**
        * Warns when non-string types are used in template expressions but allows numbers
        *
        * 🚫 Not fixable - https://typescript-eslint.io/rules/restrict-template-expressions/
--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -52,6 +52,21 @@ export const config = [
     },
     rules: {
       /**
+       * This configuration rule is part of the `@typescript-eslint` package.
+       *
+       * It disables the rule that enforces the removal of unnecessary type parameters in TypeScript.
+       *
+       * By turning off '`no-unnecessary-type-parameters`', developers can define type parameters in functions,
+       * classes, or interfaces even when those parameters are not strictly required for type inference or checking.
+       *
+       * This can be useful in scenarios where explicit type parameters are used for consistency,
+       * readability, or future-proofing code even if they are not technically required by the current implementation.
+       *
+       * https://typescript-eslint.io/rules/no-unnecessary-type-parameters/
+       */
+      '@typescript-eslint/no-unnecessary-type-parameters': 'off',
+
+      /**
        * Disables the rule that enforces using nullish coalescing operator
        *
        * https://typescript-eslint.io/rules/prefer-nullish-coalescing/
