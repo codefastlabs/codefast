4569d7f11b1667e358a26cb9e516eca1a99ace23
BM Valentino <valentino@brickmate.vn>
2024-12-09 12:03:35 +0700

    fix: use optional chaining for plugin configurations
    Replaced direct accesses with optional chaining for plugin configurations to prevent potential errors when accessing nested properties. Additionally, revised comments to use a shortened form of "doesn't" for improved readability. These changes enhance code safety and maintain consistency in style across the codebase.

--- packages/style-guide/src/configs/utils/react-hooks.ts
+++ packages/style-guide/src/configs/utils/react-hooks.ts
@@ -1,4 +1,4 @@
-// @ts-expect-error: Library does not yet support TypeScript, awaiting update or @types support
+// @ts-expect-error: Library doesn't yet support TypeScript, awaiting update or @types support
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import { type Linter } from 'eslint';
 
