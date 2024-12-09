b47f4d0c4a690d79b0b571c209740cfab2ede7c2
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-12-09 18:47:23 +0700

    feat(style-guide): enhance sorting rules with natural order
    Update 'perfectionist/sort-enums' and 'perfectionist/sort-objects' rules to use natural sorting type. Refactor imports and configuration in perfectionist.ts to include the perfectionist plugin explicitly. These changes improve the clarity and maintainability of the code style guide configuration.

--- packages/style-guide/src/configs/utils/perfectionist.ts
+++ packages/style-guide/src/configs/utils/perfectionist.ts
@@ -1,10 +1,12 @@
 import type { Linter } from 'eslint';
-import perfectionist from 'eslint-plugin-perfectionist';
+import perfectionistPlugin from 'eslint-plugin-perfectionist';
 
 import { perfectionistRules } from '@/rules/perfectionist';
 
 export const perfectionistConfig: Linter.Config = {
-  ...perfectionist.configs['recommended-natural'],
+  plugins: {
+    perfectionist: perfectionistPlugin,
+  },
   rules: {
     ...perfectionistRules.rules,
   },
