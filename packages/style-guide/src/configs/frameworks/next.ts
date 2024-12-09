4ba84939519fb3dbfd407378a2993a2b920c2a23
BM Valentino <valentino@brickmate.vn>
2024-12-09 12:03:46 +0700

    feat(style-guide): enhance ESLint config for Next.js
    Add ESLint plugins for import, accessibility, and React to the Next.js configuration. This includes recommended rules and specific warnings for unnamed default exports and accessibility standards. The configuration aims to improve code quality and maintainability in Next.js projects.

--- packages/style-guide/src/configs/frameworks/next.ts
+++ packages/style-guide/src/configs/frameworks/next.ts
@@ -1,5 +1,10 @@
-// @ts-expect-error: Library does not yet support TypeScript, awaiting update or @types support
+// @ts-expect-error: Library doesn't yet support TypeScript, awaiting update or @types support
 import nextPlugin from '@next/eslint-plugin-next';
+// @ts-expect-error: Library doesn't yet support TypeScript, awaiting update or @types support
+import importPlugin from 'eslint-plugin-import';
+// @ts-expect-error: Library doesn't yet support TypeScript, awaiting update or @types support
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import reactPlugin from 'eslint-plugin-react';
 import { type Linter } from 'eslint';
 
 export const nextConfig: Linter.Config[] = [
@@ -10,6 +15,35 @@ export const nextConfig: Linter.Config[] = [
     rules: {
       ...nextPlugin.configs.recommended.rules,
       ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+  {
+    ...importPlugin.flatConfigs.recommended,
+    rules: {
+      'import/no-anonymous-default-export': 'warn',
+    },
+  },
+  {
+    ...jsxA11yPlugin.flatConfigs.recommended,
+    rules: {
+      'jsx-a11y/alt-text': [
+        'warn',
+        {
+          elements: ['img'],
+          img: ['Image'],
+        },
+      ],
+      'jsx-a11y/aria-props': 'warn',
+      'jsx-a11y/aria-proptypes': 'warn',
+      'jsx-a11y/aria-unsupported-elements': 'warn',
+      'jsx-a11y/role-has-required-aria-props': 'warn',
+      'jsx-a11y/role-supports-aria-props': 'warn',
+    },
+  },
+  {
+    ...reactPlugin.configs.flat?.recommended,
+    rules: {
+      'react/jsx-no-target-blank': 'off',
     },
   },
 ];
