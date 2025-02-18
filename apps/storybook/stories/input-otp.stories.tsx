f3cddabebd6a6b87382fbe966998f6aad3acc8ec
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-02-18 18:49:38 +0700

    feat(storybook): add disabled variants for components
    Added a disabled variant to CheckboxCardsItem and a new disabled story for InputOTP. This improves component documentation and showcases their behavior in a disabled state.

--- apps/storybook/stories/input-otp.stories.tsx
+++ apps/storybook/stories/input-otp.stories.tsx
@@ -104,6 +104,31 @@ export const SeparatorOTP: Story = {
 };
 
 /* -----------------------------------------------------------------------------
+ * Story: Disabled
+ * -------------------------------------------------------------------------- */
+
+export const Disabled: Story = {
+  render: () => (
+    <InputOTP disabled maxLength={6}>
+      <InputOTPGroup>
+        <InputOTPSlot index={0} />
+        <InputOTPSlot index={1} />
+      </InputOTPGroup>
+      <InputOTPSeparator />
+      <InputOTPGroup>
+        <InputOTPSlot index={2} />
+        <InputOTPSlot index={3} />
+      </InputOTPGroup>
+      <InputOTPSeparator />
+      <InputOTPGroup>
+        <InputOTPSlot index={4} />
+        <InputOTPSlot index={5} />
+      </InputOTPGroup>
+    </InputOTP>
+  ),
+};
+
+/* -----------------------------------------------------------------------------
  * Story: Controlled
  * -------------------------------------------------------------------------- */
 
