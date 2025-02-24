d6aceccca54d6950819fa161c1eea8e0f713297e
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-02-24 13:44:06 +0700

    refactor(storybook): simplify addon and framework paths
    Removed the custom `getAbsolutePath` function and replaced it with direct package paths for Storybook addons and the framework. This simplifies the configuration and reduces unnecessary complexity.

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,28 +1,19 @@
 import type { StorybookConfig } from '@storybook/nextjs';
-import { dirname, join } from 'path';
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is necessary in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
 
 const config: StorybookConfig = {
   addons: [
-    getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath('@storybook/addon-themes'),
-    getAbsolutePath('@chromatic-com/storybook'),
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-themes',
+    '@chromatic-com/storybook',
   ],
   core: {
     disableWhatsNewNotifications: true,
     disableTelemetry: true,
   },
   framework: {
-    name: getAbsolutePath('@storybook/nextjs'),
+    name: '@storybook/nextjs',
     options: {},
   },
   staticDirs: ['../public'],
