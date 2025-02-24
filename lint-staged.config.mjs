3fc098fc7fd62e37d41b1200bc59e51dc7b28c0c
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-02-24 13:44:54 +0700

    chore: update Prettier and lint-staged file patterns
    Expanded file patterns in Prettier configuration to include CSS and SCSS, and updated lint-staged to handle these formats consistently. This ensures better formatting and linting coverage across additional file types.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 const config = {
   '*.{js,mjs,jsx,ts,tsx}': ['prettier --list-different --write'],
-  '*.{json,md}': ['prettier --list-different --write'],
+  '*.{json,css,scss,md}': ['prettier --list-different --write'],
 };
 
 export default config;
