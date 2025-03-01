715e30dd998fa050fcd141ca534fa281f1b34423
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-03-01 08:23:40 +0700

    feat(style-guide): consolidate and enhance Prettier support
    Added a new Prettier rules configuration to centralize rule definitions. Updated Prettier and React Hooks configs to use the enhanced rule setup for improved maintainability and clarity.

--- packages/style-guide/src/configs/utils/react-hooks.ts
+++ packages/style-guide/src/configs/utils/react-hooks.ts
@@ -1,14 +1,11 @@
 import type { Linter } from 'eslint';
 
-// @ts-expect-error: Library doesn't yet support TypeScript, awaiting update or @types support
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
 
 export const reactHooksConfig: Linter.Config = {
+  ...eslintPluginReactHooks.configs['recommended-latest'],
   name: '@codefast/style-guide/configs/utils/react-hooks',
-  plugins: {
-    'react-hooks': eslintPluginReactHooks,
-  },
   rules: {
-    ...eslintPluginReactHooks.configs.recommended.rules,
+    ...eslintPluginReactHooks.configs['recommended-latest'].rules,
   },
 };
