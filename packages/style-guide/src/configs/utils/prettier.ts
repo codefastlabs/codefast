2096047c232426089e205bc0431b4999623e40ac
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-03-01 08:23:40 +0700

    feat(style-guide): consolidate and enhance Prettier support
    Added a new Prettier rules configuration to centralize rule definitions. Updated Prettier and React Hooks configs to use the enhanced rule setup for improved maintainability and clarity.

--- packages/style-guide/src/configs/utils/prettier.ts
+++ packages/style-guide/src/configs/utils/prettier.ts
@@ -2,11 +2,13 @@ import type { Linter } from 'eslint';
 
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
+import { prettierRules } from '@/rules/prettier';
+
 export const prettierConfig: Linter.Config = {
   ...eslintPluginPrettier,
   name: '@codefast/style-guide/configs/utils/prettier',
   rules: {
     ...eslintPluginPrettier.rules,
-    'prettier/prettier': 'warn',
+    ...prettierRules.rules,
   },
 };
