1b9b8cf746df10778790f5ce555820974add408b
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-03-04 22:11:25 +0700

    feat(badge): simplify badge component by removing size variants
    Refactored the badge component to remove size-related variants and compound variants. Updated styles to use consistent padding and font sizes, simplifying maintainability. Adjusted related stories and other affected components accordingly.

--- packages/ui/src/components/badge.tsx
+++ packages/ui/src/components/badge.tsx
@@ -8,16 +8,8 @@ import { tv } from 'tailwind-variants';
  * -------------------------------------------------------------------------- */
 
 const badgeVariants = tv({
-  base: 'inline-flex shrink-0 items-center justify-center gap-2 whitespace-nowrap rounded-lg font-semibold [&>svg]:shrink-0',
+  base: 'inline-flex shrink-0 items-center justify-center gap-2 whitespace-nowrap rounded-md px-1.5 py-0.5 text-sm/5 font-medium sm:text-xs/5 [&>svg]:shrink-0',
   variants: {
-    icon: { false: '', true: 'px-0' },
-    size: {
-      xs: 'px-2.25 h-5 text-xs [&>svg]:size-3', // 20px
-      sm: 'h-6 px-2.5 text-xs [&>svg]:size-3', // 24px
-      md: 'px-2.75 h-7 text-sm [&>svg]:size-4', // 28px
-      lg: 'h-8 px-3 text-sm [&>svg]:size-5', // 32px
-      xl: 'px-3.25 h-9 text-sm [&>svg]:size-5', // 36px
-    },
     variant: {
       default: 'bg-primary text-primary-foreground',
       secondary: 'bg-secondary text-secondary-foreground',
@@ -29,37 +21,8 @@ const badgeVariants = tv({
     },
   },
   defaultVariants: {
-    icon: false,
-    size: 'sm',
     variant: 'default',
   },
-  compoundVariants: [
-    {
-      className: 'w-8',
-      icon: true,
-      size: 'xs',
-    },
-    {
-      className: 'w-9',
-      icon: true,
-      size: 'sm',
-    },
-    {
-      className: 'w-10',
-      icon: true,
-      size: 'md',
-    },
-    {
-      className: 'w-11',
-      icon: true,
-      size: 'lg',
-    },
-    {
-      className: 'w-12',
-      icon: true,
-      size: 'xl',
-    },
-  ],
 });
 
 type BadgeVariantsProps = VariantProps<typeof badgeVariants>;
@@ -73,9 +36,9 @@ interface BadgeProps extends Omit<ComponentProps<'div'>, 'prefix'>, BadgeVariant
   suffix?: ReactNode;
 }
 
-function Badge({ children, className, prefix, size, suffix, variant, ...props }: BadgeProps): JSX.Element {
+function Badge({ children, className, prefix, suffix, variant, ...props }: BadgeProps): JSX.Element {
   return (
-    <div className={badgeVariants({ className, size, variant })} {...props}>
+    <div className={badgeVariants({ className, variant })} {...props}>
       {prefix}
       {typeof children === 'string' ? <span className="truncate">{children}</span> : children}
       {suffix}
