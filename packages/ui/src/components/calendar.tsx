6230327d2a5bea09706b7fd4efed023e866653cb
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-03-04 15:09:00 +0700

    feat(calendar): enhance styles and feature configuration
    Improved calendar component styles and added support for enhanced customization using `react-day-picker`. Refactored components, consolidated props, and introduced versatile layout options. Example configurations in the docs app have been updated to reflect and demonstrate these changes.

--- packages/ui/src/components/calendar.tsx
+++ packages/ui/src/components/calendar.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import type { ComponentProps, JSX, ReactNode } from 'react';
-import type { CustomComponents, DateRange, DayButtonProps, DayPickerProps } from 'react-day-picker';
+import type { CustomComponents, DateRange, DayPickerProps } from 'react-day-picker';
 
 import { format } from 'date-fns';
 import { ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon, ChevronUpIcon, DotIcon } from 'lucide-react';
-import { useEffect, useRef } from 'react';
 import { DayFlag, DayPicker, SelectionState, UI } from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/variants/button.variants';
 
 /* -----------------------------------------------------------------------------
  * Component: Chevron
@@ -16,56 +16,28 @@ import { cn } from '@/lib/utils';
 
 type ChevronProps = ComponentProps<CustomComponents['Chevron']>;
 
-function Chevron({ orientation, ...props }: ChevronProps): JSX.Element {
+function Chevron({ orientation, className, ...props }: ChevronProps): JSX.Element {
   switch (orientation) {
     case 'up': {
-      return <ChevronUpIcon className="size-4" {...props} />;
+      return <ChevronUpIcon className={cn('size-4', className)} {...props} />;
     }
 
     case 'down': {
-      return <ChevronDownIcon className="size-4" {...props} />;
+      return <ChevronDownIcon className={cn('size-4', className)} {...props} />;
     }
 
     case 'left': {
-      return <ChevronLeftIcon className="size-4" {...props} />;
+      return <ChevronLeftIcon className={cn('size-4', className)} {...props} />;
     }
 
     case 'right': {
-      return <ChevronRightIcon className="size-4" {...props} />;
+      return <ChevronRightIcon className={cn('size-4', className)} {...props} />;
     }
 
     default: {
-      return <DotIcon className="size-4" {...props} />;
+      return <DotIcon className={cn('size-4', className)} {...props} />;
     }
   }
-}
-
-/* -----------------------------------------------------------------------------
- * Component: DayButton
- * -------------------------------------------------------------------------- */
-
-function DayButton({ className, day: _, modifiers, ...props }: DayButtonProps): JSX.Element {
-  const buttonRef = useRef<HTMLButtonElement>(null);
-
-  useEffect(() => {
-    if (modifiers.focused) {
-      buttonRef.current?.focus();
-    }
-  }, [modifiers.focused]);
-
-  return (
-    <button
-      ref={buttonRef}
-      className={cn('', className)}
-      data-focused={modifiers.focused}
-      data-hidden={modifiers.hidden}
-      data-outside={modifiers.outside}
-      data-today={modifiers.today}
-      disabled={modifiers.disabled}
-      type="button"
-      {...props}
-    />
-  );
 }
 
 /* -----------------------------------------------------------------------------
@@ -75,48 +47,84 @@ function DayButton({ className, day: _, modifiers, ...props }: DayButtonProps): 
 type CalendarProps = DayPickerProps;
 
 function Calendar({ className, classNames, ...props }: CalendarProps): JSX.Element {
+  const isInteractive = props.mode !== undefined || props.onDayClick !== undefined;
+
   return (
     <DayPicker
-      showOutsideDays
       className={cn('', className)}
       classNames={{
         [UI.Root]: cn('UI.Root', '', classNames?.[UI.Root]),
         [UI.Chevron]: cn('UI.Chevron', '', classNames?.[UI.Chevron]),
-        [UI.Day]: cn('UI.Day', '', classNames?.[UI.Day]),
-        [UI.DayButton]: cn('UI.DayButton', '', classNames?.[UI.DayButton]),
-        [UI.CaptionLabel]: cn('UI.CaptionLabel', '', classNames?.[UI.CaptionLabel]),
-        [UI.Dropdowns]: cn('UI.Dropdowns', '', classNames?.[UI.Dropdowns]),
-        [UI.Dropdown]: cn('UI.Dropdown', '', classNames?.[UI.Dropdown]),
-        [UI.DropdownRoot]: cn('UI.DropdownRoot', '', classNames?.[UI.DropdownRoot]),
+        [UI.Day]: cn(
+          'UI.Day',
+          'grid place-items-center text-center text-sm',
+          isInteractive ? 'min-w-8.5 group' : 'min-w-8.5 h-8',
+          classNames?.[UI.Day],
+        ),
+        [UI.DayButton]: cn(
+          'group-[.is-outside]:text-muted-foreground hover:not-disabled:not-group-aria-selected:bg-accent hover:not-disabled:not-group-aria-selected:text-accent-foreground group-data-today:not-group-aria-selected:bg-accent group-aria-selected:not-group-[.is-range-middle]:bg-primary group-aria-selected:not-group-[.is-range-middle]:text-primary-foreground size-8 rounded-lg transition disabled:opacity-50',
+          classNames?.[UI.DayButton],
+        ),
+        [UI.CaptionLabel]: cn(
+          'UI.CaptionLabel',
+          'flex items-center gap-2 [&>svg]:opacity-50',
+          classNames?.[UI.CaptionLabel],
+        ),
+        [UI.Dropdowns]: cn('UI.Dropdowns', 'flex gap-4', classNames?.[UI.Dropdowns]),
+        [UI.Dropdown]: cn('UI.Dropdown', 'absolute opacity-0', classNames?.[UI.Dropdown]),
+        [UI.DropdownRoot]: cn('UI.DropdownRoot', 'relative', classNames?.[UI.DropdownRoot]),
         [UI.Footer]: cn('UI.Footer', '', classNames?.[UI.Footer]),
         [UI.MonthGrid]: cn('UI.MonthGrid', '', classNames?.[UI.MonthGrid]),
-        [UI.MonthCaption]: cn('UI.MonthCaption', '', classNames?.[UI.MonthCaption]),
+        [UI.MonthCaption]: cn('UI.MonthCaption', 'py-1 text-sm font-medium', classNames?.[UI.MonthCaption]),
         [UI.MonthsDropdown]: cn('UI.MonthsDropdown', '', classNames?.[UI.MonthsDropdown]),
-        [UI.Month]: cn('UI.Month', '', classNames?.[UI.Month]),
-        [UI.Months]: cn('UI.Months', '', classNames?.[UI.Months]),
-        [UI.Nav]: cn('UI.Nav', '', classNames?.[UI.Nav]),
-        [UI.NextMonthButton]: cn('UI.NextMonthButton', '', classNames?.[UI.NextMonthButton]),
-        [UI.PreviousMonthButton]: cn('UI.PreviousMonthButton', '', classNames?.[UI.PreviousMonthButton]),
-        [UI.Week]: cn('UI.Week', '', classNames?.[UI.Week]),
+        [UI.Month]: cn('UI.Month', 'flex flex-col gap-4', classNames?.[UI.Month]),
+        [UI.Months]: cn('UI.Months', 'relative inline-flex flex-wrap gap-4', classNames?.[UI.Months]),
+        [UI.Nav]: cn('UI.Nav', 'absolute end-0 flex gap-2', classNames?.[UI.Nav]),
+        [UI.NextMonthButton]: cn(
+          'UI.NextMonthButton',
+          buttonVariants({ variant: 'ghost', className: '', size: '2xs', icon: true }),
+          classNames?.[UI.NextMonthButton],
+        ),
+        [UI.PreviousMonthButton]: cn(
+          'UI.PreviousMonthButton',
+          buttonVariants({ variant: 'ghost', className: '', size: '2xs', icon: true }),
+          classNames?.[UI.PreviousMonthButton],
+        ),
+        [UI.Week]: cn('UI.Week', 'mt-2 flex justify-between', classNames?.[UI.Week]),
         [UI.Weeks]: cn('UI.Weeks', '', classNames?.[UI.Weeks]),
-        [UI.Weekday]: cn('UI.Weekday', '', classNames?.[UI.Weekday]),
-        [UI.Weekdays]: cn('UI.Weekdays', '', classNames?.[UI.Weekdays]),
-        [UI.WeekNumber]: cn('UI.WeekNumber', '', classNames?.[UI.WeekNumber]),
-        [UI.WeekNumberHeader]: cn('UI.WeekNumberHeader', '', classNames?.[UI.WeekNumberHeader]),
+        [UI.Weekday]: cn('UI.Weekday', 'text-muted-foreground min-w-8.5 text-xs font-medium', classNames?.[UI.Weekday]),
+        [UI.Weekdays]: cn('UI.Weekdays', 'flex w-full justify-between', classNames?.[UI.Weekdays]),
+        [UI.WeekNumber]: cn(
+          'UI.WeekNumber',
+          'text-muted-foreground min-w-8.5 grid h-8 place-items-center text-xs font-normal [&+*]:rounded-l-lg',
+          classNames?.[UI.WeekNumber],
+        ),
+        [UI.WeekNumberHeader]: cn('UI.WeekNumberHeader', 'min-w-8.5', classNames?.[UI.WeekNumberHeader]),
         [UI.YearsDropdown]: cn('UI.YearsDropdown', '', classNames?.[UI.YearsDropdown]),
 
-        [SelectionState.range_end]: cn('SelectionState.range_end', '', classNames?.[SelectionState.range_end]),
-        [SelectionState.range_middle]: cn('SelectionState.range_middle', '', classNames?.[SelectionState.range_middle]),
-        [SelectionState.range_start]: cn('SelectionState.range_start', '', classNames?.[SelectionState.range_start]),
-        [SelectionState.selected]: cn('SelectionState.selected', '', classNames?.[SelectionState.selected]),
+        [SelectionState.range_end]: cn(
+          'is-range-end bg-accent rounded-r-lg first:rounded-l-lg',
+          classNames?.[SelectionState.range_end],
+        ),
+        [SelectionState.range_middle]: cn(
+          'is-range-middle bg-accent first:rounded-l-lg last:rounded-r-lg',
+          classNames?.[SelectionState.range_middle],
+        ),
+        [SelectionState.range_start]: cn(
+          'is-range-start bg-accent rounded-l-lg last:rounded-r-lg',
+          classNames?.[SelectionState.range_start],
+        ),
+        [SelectionState.selected]: cn('is-selected', classNames?.[SelectionState.selected]),
 
-        [DayFlag.disabled]: cn('DayFlag.disabled', '', classNames?.[DayFlag.disabled]),
-        [DayFlag.focused]: cn('DayFlag.focused', '', classNames?.[DayFlag.focused]),
-        [DayFlag.hidden]: cn('DayFlag.hidden', '', classNames?.[DayFlag.hidden]),
-        [DayFlag.outside]: cn('DayFlag.outside', '', classNames?.[DayFlag.outside]),
-        [DayFlag.today]: cn('DayFlag.today', '', classNames?.[DayFlag.today]),
+        [DayFlag.disabled]: cn('is-disabled', classNames?.[DayFlag.disabled]),
+        [DayFlag.focused]: cn('is-focused', classNames?.[DayFlag.focused]),
+        [DayFlag.hidden]: cn('is-hidden size-8', classNames?.[DayFlag.hidden]),
+        [DayFlag.outside]: cn('is-outside', classNames?.[DayFlag.outside]),
+        [DayFlag.today]: cn('is-today', classNames?.[DayFlag.today]),
       }}
-      components={{ Chevron, DayButton }}
+      components={{
+        Chevron,
+      }}
       {...props}
     />
   );
