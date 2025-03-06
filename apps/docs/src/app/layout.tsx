8546daea5e317db16ebfe0c534f02e9feff76b85
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-03-06 22:24:07 +0700

    feat(docs): integrate shared UI and linting configs
    Replaced custom ESLint configuration with `@codefast/eslint-config` for consistency and simplified maintenance. Enhanced UI by integrating `@codefast/ui` components, styles, and updated Tailwind config. Adjusted package scripts and dependencies for streamlined development workflows.

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,13 +1,17 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { JSX } from 'react';
+
+import { cn } from '@codefast/ui';
+import { Geist, Geist_Mono as GeistMono } from 'next/font/google';
+
+import '@/app/globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = GeistMono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
@@ -21,10 +25,10 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>): JSX.Element {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html className={cn(geistSans.variable, geistMono.variable, 'antialiased')} lang="en">
+      <body>{children}</body>
     </html>
   );
 }
