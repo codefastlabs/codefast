cb70e3cf7c5f0b90ae7e656fa4153f15112ecaec
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-03-09 21:35:30 +0700

    feat(docs): add chart demos to app page
    Introduce multiple chart examples (Area, Bar, Line) with reusable components to enhance data visualization. Updated the app page to include these components wrapped in a responsive layout.

--- apps/docs/src/app/(app)/page.tsx
+++ apps/docs/src/app/(app)/page.tsx
@@ -1,10 +1,19 @@
 import { type Metadata } from 'next';
 import { type JSX } from 'react';
 
+import { ChartDemo } from '@/components/chart-demo';
+import { ComponentWrapper } from '@/components/component-wrapper';
+
 export const metadata: Metadata = {
   title: 'App',
 };
 
 export default function AppPage(): JSX.Element {
-  return <div className="@container grid flex-1 gap-4 p-4">App Page</div>;
+  return (
+    <div className="@container grid flex-1 gap-4 p-4">
+      <ComponentWrapper className="w-full" name="chart">
+        <ChartDemo />
+      </ComponentWrapper>
+    </div>
+  );
 }
