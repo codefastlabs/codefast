d752615a97ec633a08806cbf7ff4c2f90221b05d
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-03-22 19:55:01 +0700

    feat(sidebar): add ScrollArea for enhanced scrolling support
    Replaced the root container of SidebarContent with ScrollArea to handle scrolling more effectively. Refactored ScrollArea component by renaming context methods and improving class usage. This improves modularity, readability, and user experience for scrollable regions.

--- packages/ui/src/components/sidebar.tsx
+++ packages/ui/src/components/sidebar.tsx
@@ -11,6 +11,7 @@ import { tv } from 'tailwind-variants';
 
 import { Button } from '@/components/button';
 import { Input } from '@/components/input';
+import { ScrollArea } from '@/components/scroll-area';
 import { Separator } from '@/components/separator';
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from '@/components/sheet';
 import { Skeleton } from '@/components/skeleton';
@@ -383,15 +384,17 @@ function SidebarSeparator({ className, ...props }: ComponentProps<typeof Separat
 
 function SidebarContent({ className, ...props }: ComponentProps<'div'>): JSX.Element {
   return (
-    <div
-      className={cn(
-        'flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden',
-        className,
-      )}
-      data-sidebar="content"
-      data-slot="sidebar-content"
-      {...props}
-    />
+    <ScrollArea className="grow overflow-auto">
+      <div
+        className={cn(
+          'flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden',
+          className,
+        )}
+        data-sidebar="content"
+        data-slot="sidebar-content"
+        {...props}
+      />
+    </ScrollArea>
   );
 }
 
@@ -426,7 +429,7 @@ function SidebarGroupLabel({
   return (
     <Component
       className={cn(
-        'text-sidebar-foreground/70 ring-sidebar-ring outline-hidden focus-visible:ring-3 flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium transition-[margin,opacity] duration-200 ease-linear group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0 [&>svg]:size-4 [&>svg]:shrink-0',
+        'text-sidebar-foreground/70 ring-sidebar-ring outline-hidden focus-visible:ring-3 flex h-8 shrink-0 items-center truncate rounded-md px-2 text-xs font-medium transition-[margin,opacity] duration-200 ease-linear group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0 [&>svg]:size-4 [&>svg]:shrink-0',
         className,
       )}
       data-sidebar="group-label"
