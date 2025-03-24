b2f9bab551374a389200574cdeb0ff26f9fd625e
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-03-24 13:15:49 +0700

    feat(demos): add responsive toggle group with device icons
    Implemented a new toggle group demo to switch between desktop, tablet, and mobile views using device icons. Added a separator and a fullscreen link to enhance functionality and usability. Classes and styles were adjusted to ensure consistent design and accessibility.

--- apps/docs/src/components/demos/toggle-group-demo.tsx
+++ apps/docs/src/components/demos/toggle-group-demo.tsx
@@ -1,7 +1,16 @@
 import type { JSX } from 'react';
 
-import { ToggleGroup, ToggleGroupItem } from '@codefast/ui';
-import { BoldIcon, ItalicIcon, UnderlineIcon } from 'lucide-react';
+import { buttonVariants, Separator, ToggleGroup, ToggleGroupItem } from '@codefast/ui';
+import {
+  BoldIcon,
+  FullscreenIcon,
+  ItalicIcon,
+  MonitorIcon,
+  SmartphoneIcon,
+  TabletIcon,
+  UnderlineIcon,
+} from 'lucide-react';
+import Link from 'next/link';
 
 import { GridWrapper } from '@/components/grid-wrapper';
 
@@ -66,6 +75,38 @@ export function ToggleGroupDemo(): JSX.Element {
             Last 7 days
           </ToggleGroupItem>
         </ToggleGroup>
+      </div>
+      <div>
+        <div className="items-center rounded-lg border p-1">
+          <ToggleGroup
+            className="gap-1 *:rounded-sm *:first:rounded-l-sm *:last:rounded-r-sm"
+            defaultValue="100"
+            type="single"
+          >
+            <ToggleGroupItem className="size-6 min-w-0 p-0" title="Desktop" value="100">
+              <MonitorIcon className="size-4" />
+            </ToggleGroupItem>
+            <ToggleGroupItem className="size-6 min-w-0 p-0" title="Tablet" value="60">
+              <TabletIcon className="size-4" />
+            </ToggleGroupItem>
+            <ToggleGroupItem className="size-6 min-w-0 p-0" title="Mobile" value="30">
+              <SmartphoneIcon className="size-4" />
+            </ToggleGroupItem>
+            <Separator className="h-4.5" orientation="vertical" />
+            <Link
+              className={buttonVariants({
+                className: 'size-6 p-0',
+                variant: 'ghost',
+                size: 'icon',
+              })}
+              href="#"
+              title="Open in New Tab"
+            >
+              <span className="sr-only">Open in New Tab</span>
+              <FullscreenIcon className="size-4" />
+            </Link>
+          </ToggleGroup>
+        </div>
       </div>
     </GridWrapper>
   );
