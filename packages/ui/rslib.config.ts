75079fe651d0e1dc5592efd879416c264425084a
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-04-13 13:23:07 +0700

    chore(build): migrate from `tsup` to `rslib` for build process
    Replaced `tsup` with `rslib` across all packages to improve build configuration clarity and adapt to the new tooling. Updated configuration files and scripts, and introduced `rslib.config.ts` for managing build options. Adjusted package.json settings accordingly.

---
+++ packages/ui/rslib.config.ts
@@ -0,0 +1,40 @@
+import { pluginReact } from "@rsbuild/plugin-react";
+import { defineConfig } from "@rslib/core";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default defineConfig({
+  lib: [
+    {
+      bundle: false,
+      dts: true,
+      format: "esm",
+      output: {
+        distPath: {
+          root: "./dist/esm",
+        },
+      },
+    },
+    {
+      bundle: false,
+      dts: true,
+      format: "cjs",
+      output: {
+        distPath: {
+          root: "./dist/cjs",
+        },
+      },
+    },
+  ],
+  output: {
+    cleanDistPath: isProduction,
+    target: "web",
+    copy: ["./src/styles/index.css"],
+  },
+  plugins: [pluginReact()],
+  source: {
+    entry: {
+      index: ["./src/**/*.{ts,tsx}", "!./src/**/*.{test,spec}.*"],
+    },
+  },
+});
