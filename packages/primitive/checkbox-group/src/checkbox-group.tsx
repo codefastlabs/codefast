b340d0ec3311b9466bd8c1a772a8560b06f8a1fc
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-04-13 11:50:16 +0700

    feat(checkbox-group): refine exports for better tree-shaking
    Replaced wildcard exports with named exports to improve tree-shaking and reduce bundle size. Adjusted the index file to explicitly define and export components and utilities from the checkbox-group module.

--- packages/primitive/checkbox-group/src/checkbox-group.tsx
+++ packages/primitive/checkbox-group/src/checkbox-group.tsx
@@ -322,6 +322,7 @@ function CheckboxGroupIndicator({
 /* -----------------------------------------------------------------------------
  * Exports
  * -------------------------------------------------------------------------- */
+
 export {
   CheckboxGroup,
   CheckboxGroupIndicator,
