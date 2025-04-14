228734853007f387aaee2f64d80d63cb1ce4ad04
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-04-14 14:27:39 +0700

    feat(update-exports): enhance packages glob and add ignore option
    Updated the packages glob pattern for improved flexibility and added an ignore option to exclude "node_modules" from the search. Removed the "start" script from `update-exports` package.json, as it is unnecessary. Added a new root script for running the update-exports functionality.

--- scripts/update-exports/src/config/default-config.ts
+++ scripts/update-exports/src/config/default-config.ts
@@ -2,7 +2,7 @@ import type { ScriptConfig } from "@/types/config";
 
 // Cấu hình mặc định
 export const defaultConfig: ScriptConfig = {
-  packagesGlob: "./packages/*/package.json",
+  packagesGlob: "./packages/**/package.json",
   defaultPackageConfig: {
     srcIndexPath: "src/index.ts",
     packageJsonPath: "package.json",
