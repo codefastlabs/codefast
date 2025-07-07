d8c0d6244b851b0d4886faec7128437ec61f8528
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-07-07 22:46:15 +0700

    chore(config): add commitlint configuration for enforcing commit message standards
    Introduced a `commitlint.config.ts` file to standardize commit messages across the repository. Configured rules include types, subject length, and line lengths, ensuring adherence to conventional commit guidelines. This change enhances consistency and clarity in commit history.

---
+++ commitlint.config.ts
@@ -0,0 +1,21 @@
+import type { UserConfig } from "@commitlint/types";
+
+const ERROR_LEVEL = 2;
+const MAX_SUBJECT_LENGTH = 100;
+const MAX_BODY_LINE_LENGTH = 500;
+
+const config: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      ERROR_LEVEL,
+      "always",
+      ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"],
+    ],
+    "subject-case": [ERROR_LEVEL, "never", ["pascal-case", "upper-case"]],
+    "subject-max-length": [ERROR_LEVEL, "always", MAX_SUBJECT_LENGTH],
+    "body-max-line-length": [ERROR_LEVEL, "always", MAX_BODY_LINE_LENGTH],
+  },
+};
+
+export default config;
