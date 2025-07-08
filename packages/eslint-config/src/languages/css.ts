6802efe78c9a949e057fd229e0a8eccf4b7d134b
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-07-08 23:37:37 +0700

    chore(config): add CSS linting rules and dependencies to ESLint configuration
    Included the `@eslint/css` package and added recommended CSS linting rules to the `eslint-config` package. Updated lockfile and dependencies, introduced `cssRules`, and exported it for broader usage. Adjusted ignored paths in JavaScript rules to include `.next` directory for consistency.

---
+++ packages/eslint-config/src/languages/css.ts
@@ -0,0 +1,15 @@
+import css from "@eslint/css";
+
+import type { Linter } from "eslint";
+
+// CSS file configuration
+export const cssRules: Linter.Config[] = [
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    rules: {
+      ...css.configs.recommended.rules,
+    },
+  },
+];
