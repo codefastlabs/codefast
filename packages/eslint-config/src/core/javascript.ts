0609582858268bbb108f09edbe403a870920f896
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-07-13 18:28:18 +0700

    chore(rules): enforce blank line padding in ESLint configuration for improved readability
    Added `padding-line-between-statements` rule to ensure consistent blank line spacing between specific statement types such as variable declarations, directives, and return statements. This enhances code readability and aligns with best practices for code structure and maintainability.

--- packages/eslint-config/src/core/javascript.ts
+++ packages/eslint-config/src/core/javascript.ts
@@ -43,6 +43,49 @@ export const baseJavaScriptRules: Linter.Config[] = [
       "prefer-template": "error",
       radix: "error",
       yoda: "error",
+
+      // Spacing rules
+      "padding-line-between-statements": [
+        "error",
+
+        // Add blank lines between variable declarations and other statements
+        {
+          blankLine: "always",
+          next: "*",
+          prev: ["const", "let", "var"],
+        },
+        {
+          blankLine: "any",
+          next: ["const", "let", "var"],
+          prev: ["const", "let", "var"],
+        },
+
+        // Add blank lines between directives and other statements
+        {
+          blankLine: "always",
+          next: "*",
+          prev: "directive",
+        },
+        {
+          blankLine: "any",
+          next: "directive",
+          prev: "directive",
+        },
+
+        // Add blank lines between case/default statements
+        {
+          blankLine: "always",
+          next: "*",
+          prev: ["case", "default"],
+        },
+
+        // Add blank lines before return statements
+        {
+          blankLine: "always",
+          next: "return",
+          prev: "*",
+        },
+      ],
     },
   },
 ];
