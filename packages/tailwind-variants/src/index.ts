453b3dc8236435de61e6b900badc879554040094
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-08-16 11:39:06 +0700

    refactor(tailwind-variants): relocate MergeConfig type to index.ts and remove config.ts
    Moved the MergeConfig type and related TWMConfig interface from config.ts to index.ts, streamlining the structure of the Tailwind Variants package. The config.ts file has been deleted as its contents are now integrated into index.ts, enhancing code organization and clarity.

--- packages/tailwind-variants/src/index.ts
+++ packages/tailwind-variants/src/index.ts
@@ -1,9 +1,34 @@
 import type { ClassNameValue as ClassValue } from "tailwind-merge";
 
-import type { TVConfig, TWMConfig } from "@/config";
-
 import { createTwMerge } from "@/cn";
 import { falsyToString, flatMergeArrays, isEmptyObject, isEqual, mergeObjects } from "@/utils";
+
+/**
+ * ----------------------------------------
+ * Config Types
+ * ----------------------------------------
+ */
+
+import type { ConfigExtension } from "tailwind-merge";
+
+export type MergeConfig = ConfigExtension<string, string>;
+
+export interface TWMConfig {
+  /**
+   * Whether to merge the class names with `tailwind-merge` library.
+   * It's avoid to have duplicate tailwind classes. (Recommended)
+   * @see https://github.com/dcastil/tailwind-merge/blob/v2.2.0/README.md
+   * @default true
+   */
+  twMerge?: boolean;
+  /**
+   * The config object for `tailwind-merge` library.
+   * @see https://github.com/dcastil/tailwind-merge/blob/v2.2.0/docs/configuration.md
+   */
+  twMergeConfig?: MergeConfig;
+}
+
+export type TVConfig = TWMConfig;
 
 /**
  * ----------------------------------------
