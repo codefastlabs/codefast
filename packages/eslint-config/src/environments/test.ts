aad8a5a0e8fdcb4f04344846d18f4c8aa140beb8
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-08-27 23:24:43 +0700

    refactor(test-config): update `testMatch` and cleanup ignored directories for testing consistency
    Unified the `testMatch` patterns in Jest configuration files across multiple packages to improve consistency and readability. Adjusted ignored directories in build configuration to streamline test file detection and ensure alignment with updated patterns. This enhances maintainability and simplifies the testing setup.

--- packages/eslint-config/src/environments/test.ts
+++ packages/eslint-config/src/environments/test.ts
@@ -4,7 +4,7 @@ import globals from "globals";
 
 export const testEnvironment: Linter.Config[] = [
   {
-    files: ["**/__tests__/**/*.?([mc])[jt]s?(x)", "**/?(*.)+(spec|test).?([mc])[jt]s?(x)"],
+    files: ["**/__tests__/**/*.?([mc])[jt]s?(x)", "**/*.(spec|test).?([mc])[jt]s?(x)"],
     languageOptions: {
       globals: {
         ...globals.jest,
--- packages/tailwind-variants/jest.config.ts
+++ packages/tailwind-variants/jest.config.ts
@@ -31,7 +31,7 @@ const config: Config = {
   /**
    * The glob patterns Jest uses to detect test files
    */
-  testMatch: ["<rootDir>/tests/unit/**/*.?([mc])[jt]s?(x)", "**/?(*.)+(spec|test).?([mc])[jt]s?(x)"],
+  testMatch: ["<rootDir>/tests/unit/**/*.?([mc])[jt]s?(x)", "**/*.(spec|test).?([mc])[jt]s?(x)"],
 
   /**
    * The glob patterns Jest uses to ignore test files
