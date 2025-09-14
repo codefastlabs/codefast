98c5e61e0957f19f485d366d3c817d182e2f1bb6
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-09-14 21:36:52 +0700

    refactor(commitlint-config): replace subject rules with header rules for improved consistency
    Updated `commitlint.config.js` by replacing `subject` specific rules with `header` rules to align with broader commit message standards. This change ensures improved clarity and uniform configuration for commitlint checks.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 const ERROR_LEVEL = 2;
-const MAX_SUBJECT_LENGTH = 250;
+const MAX_HEADER_LENGTH = 250;
 const MAX_BODY_LINE_LENGTH = 500;
 
 /** @type {import("@commitlint/types").UserConfig} */
@@ -24,7 +24,7 @@ const config = {
       ],
     ],
     "subject-case": [ERROR_LEVEL, "never", ["pascal-case", "upper-case"]],
-    "subject-max-length": [ERROR_LEVEL, "always", MAX_SUBJECT_LENGTH],
+    "header-max-length": [ERROR_LEVEL, "always", MAX_HEADER_LENGTH],
     "body-max-line-length": [ERROR_LEVEL, "always", MAX_BODY_LINE_LENGTH],
   },
 };
