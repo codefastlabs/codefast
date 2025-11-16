48f2c3cbd8c18b7fa6e5eb173bad32f244e57fa4
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2025-11-16 16:08:00 +0700

    refactor: simplify eslint.config.js by removing composeConfig and directly exporting reactPreset
    Updated the eslint.config.js file to directly export reactPreset from the @codefast/eslint-config/presets/react module, eliminating the use of the composeConfig utility. This change streamlines the ESLint configuration and reduces complexity.

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,6 +1,1 @@
-import { reactPreset } from "@codefast/eslint-config/presets/react";
-import { composeConfig } from "@codefast/eslint-config/shared/composer";
-
-export default composeConfig(reactPreset, {
-  rules: {},
-});
+export { reactPreset as default } from "@codefast/eslint-config/presets/react";
